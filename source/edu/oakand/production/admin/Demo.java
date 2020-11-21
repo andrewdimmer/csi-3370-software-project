a088af6369a35b0f8c707dca3f13bd6aaad54da7
Andrew Dimmer <contactandrewd@gmail.com>
2020-11-21 12:25:48 -0500

    [Admin] Added Missing Semicolon

--- source/edu/oakand/production/admin/Demo.java
+++ source/edu/oakand/production/admin/Demo.java
@@ -3,6 +3,6 @@ package edu.oakland.production.admin;
 public class Demo {
 
   public static void main(String[] args) {
-    System.out.println("Hello World!")
+    System.out.println("Hello World!");
   }
 }
