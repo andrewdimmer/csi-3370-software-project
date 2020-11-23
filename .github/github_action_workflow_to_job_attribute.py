6f36284835ef7bd30d6cf5d4b17d96ed01965653
Andrew Dimmer <contactandrewd@gmail.com>
2020-11-23 00:33:53 -0500

    Added Better Logging to PlantUML Generation Workflow

--- .github/github_action_workflow_to_job_attribute.py
+++ .github/github_action_workflow_to_job_attribute.py
@@ -2,6 +2,6 @@ import json
 import sys
 obj = json.load(sys.stdin)
 for job in obj["check_runs"]:
-    if job["name"] == sys.argv[0]:
-        print(job["id"])
+    if job["name"] == sys.argv[1]:
+        print(job[sys.argv[2]])
         break
