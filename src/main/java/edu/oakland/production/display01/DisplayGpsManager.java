7ba04d721e049be3efaef136e24778425e4798b1
atpoynter <55966011+atpoynter@users.noreply.github.com>
2020-11-24 12:58:07 -0500

    [Display01] Corrected typos and class mispellings for DisplayGpsManagerImplementation.java and DisplayGpsManager.java

--- src/main/java/edu/oakland/production/display01/DisplayGpsManager.java
+++ src/main/java/edu/oakland/production/display01/DisplayGpsManager.java
@@ -1,11 +1,11 @@
-package edu.oakland.production;
+package edu.oakland.production.display01;
 
 import edu.oakland.helper.admin.Satellite;
 import edu.oakland.helper.display01.SatelliteSignalCheckRequest;
 
 public interface DisplayGpsManager {
 
-  public String receiveGpsSignalStrengths(Satellite satelliteSignal);
+  public String receiveGpsSignalStrength(Satellite satelliteSignal);
 
   public SatelliteSignalCheckRequest passGpsSignalStrength(String satelliteName);
 
--- src/main/java/edu/oakland/production/display01/DisplayGpsManagerImplementation.java
+++ src/main/java/edu/oakland/production/display01/DisplayGpsManagerImplementation.java
@@ -1,11 +1,11 @@
-package edu.oakland.production;
+package edu.oakland.production.display01;
 
 import edu.oakland.helper.admin.Satellite;
 import edu.oakland.helper.display01.SatelliteSignalCheckRequest;
 
-public class DisplayGpsManagerImplementation implements DisplayGpsManager{
+public class DisplayGpsManagerImplementation implements DisplayGpsManager {
     
-  public String receiveGpsSignalStrengths(Satellite satelliteSignal) {
+  public String receiveGpsSignalStrength(Satellite satelliteSignal) {
     return "";
   }
 
