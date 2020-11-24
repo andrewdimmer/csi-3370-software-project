b28672fb05e4aa87ee4ec7c015e3b435a8f369ab
juliawang2014 <59949033+juliawang2014@users.noreply.github.com>
2020-11-24 12:41:23 -0500

    Update MiddlewareGisInterface.java

--- src/main/java/edu/oakland/production/middleware01/MiddlewareGisInterface.java
+++ src/main/java/edu/oakland/production/middleware01/MiddlewareGisInterface.java
@@ -3,5 +3,7 @@ package edu.oakland.production.middleware01;
 import edu.oakland.helper.admin.Satellite;
 
 public interface MiddlewareGisInterface {
-    public String receiveGpsSignalStrength(Satellite satelliteSignal);
+    
+  public String receiveGpsSignalStrength(Satellite satelliteSignal);
+
 }
