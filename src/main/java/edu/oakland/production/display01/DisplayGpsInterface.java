f5a718d65234f25cf9d559a310910ac3dc7f7668
reedsikorskiOU <73550484+reedsikorskiOU@users.noreply.github.com>
2020-11-23 21:16:48 -0500

    [Display_01] Updated Files to Pass Google Style Guide

--- src/main/java/edu/oakland/production/display01/DisplayGpsInterface.java
+++ src/main/java/edu/oakland/production/display01/DisplayGpsInterface.java
@@ -2,11 +2,12 @@ package main.java.edu.oakland.production;
 
 public interface DisplayGpsInterface {
 
-    public void recieveGPSSingnals(Satellite satelliteSignal);
+  public void recieveGPSSingnals(Satellite satelliteSignal);
 
-    public void reportGpsSignalLoss(String satelliteName);
+  public void reportGpsSignalLoss(String satelliteName);
 
-    public void recheckSignalStrength(String satelliteName);
+  public void recheckSignalStrength(String satelliteName);
 
-    public void checkSignalStrength(String satelliteName);
+  public void checkSignalStrength(String satelliteName);
+  
 }
--- src/main/java/edu/oakland/production/display01/DisplayGpsInterfaceImplementation.java
+++ src/main/java/edu/oakland/production/display01/DisplayGpsInterfaceImplementation.java
@@ -2,20 +2,20 @@ package main.java.edu.oakland.production;
 
 public class DisplayGpsInterfaceImplemention implements DisplayGpsInterface {
 
-    public void recieveGPSSingnals(Satellite satelliteSignal) {
+  public void recieveGPSSingnals(Satellite satelliteSignal) {
 
-    }
+  }
 
-    public void reportGpsSignalLoss(String satelliteName) {
+  public void reportGpsSignalLoss(String satelliteName) {
 
-    }
+  }
 
-    public void recheckSignalStrength(String satelliteName) {
+  public void recheckSignalStrength(String satelliteName) {
 
-    }
+  }
 
-    public void checkSignalStrength(String satelliteName) {
+  public void checkSignalStrength(String satelliteName) {
 
-    }
+  }
 
 } 
