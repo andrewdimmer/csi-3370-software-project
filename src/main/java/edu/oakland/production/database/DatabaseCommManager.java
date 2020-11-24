11d32344fa8152481798bb09fc3ba0d4876131cc
Endrit <endritzenuni112@gmail.com>
2020-11-24 14:49:56 -0500

    Update DatabaseCommManager.java

--- src/main/java/edu/oakland/production/database/DatabaseCommManager.java
+++ src/main/java/edu/oakland/production/database/DatabaseCommManager.java
@@ -2,19 +2,18 @@ package edu.oakland.production.database;
 
 import edu.oakland.helper.admin.TrackData;
 import edu.oakland.helper.database;
-import edu.oakland.test.database;
 
 public interface DatabaseCommManager {
 
-    public int passGetRfidRequest();
+  public int passGetRfidRequest();
 
-    public LocationDataPoint passGetLocationDataPointRequest(int offset);
+  public LocationDataPoint passGetLocationDataPointRequest(int offset);
 
-    public TrackData passGetTrackDataRequest(int offset);
+  public TrackData passGetTrackDataRequest(int offset);
 
-    public void passStoreTrackDataRequest(TrackData trackData);
-    
-    public String passGetModeRequest();
+  public void passStoreTrackDataRequest(TrackData trackData);
+
+  public String passGetModeRequest();
 
     
 }
