09cc25874719c4323b91499c74969e7d94fccd13
Endrit <endritzenuni112@gmail.com>
2020-11-24 14:58:12 -0500

    Update DatabaseCommManager.java

--- src/main/java/edu/oakland/production/database/DatabaseCommManager.java
+++ src/main/java/edu/oakland/production/database/DatabaseCommManager.java
@@ -1,7 +1,7 @@
 package edu.oakland.production.database;
 
-import edu.oakland.helper.admin.TrackData;
 import edu.oakland.helper.admin.LocationDataPoint;
+import edu.oakland.helper.admin.TrackData;
 
 public interface DatabaseCommManager {
 
