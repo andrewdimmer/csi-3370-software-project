2a712355251df82724a72908a3b6e799cac50733
EC01EC <73436762+EC01EC@users.noreply.github.com>
2020-11-24 17:17:33 -0500

    Update DisplayGpsReceiverImplementation.java
    Added "implements DisplayGpsReceiver" to class line

--- src/main/java/edu/oakland/production/display01/DisplayGpsReceiverImplementation.java
+++ src/main/java/edu/oakland/production/display01/DisplayGpsReceiverImplementation.java
@@ -3,7 +3,7 @@ package edu.oakland.production.display01;
 import edu.oakland.helper.admin.Satellite;
 import edu.oakland.helper.display01.SatelliteSignalCheckRequest;
 
-public class DisplayGpsReceiverImplementation {
+public class DisplayGpsReceiverImplementation implements DisplayGpsReceiver {
 
   /** 
    * Measures GPS Signal Strength.
