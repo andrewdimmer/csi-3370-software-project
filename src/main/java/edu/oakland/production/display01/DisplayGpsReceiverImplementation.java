2ef1e0fdc642786135dd8ef1ceb5ce1acfc853d5
EC01EC <73436762+EC01EC@users.noreply.github.com>
2020-11-24 16:39:21 -0500

    [Display_01] Created DisplayGpsReceiver
    Added DisplayGpsReceiver and DisplayGpsReceiverImplementation

---
+++ src/main/java/edu/oakland/production/display01/DisplayGpsReceiverImplementation.java
@@ -0,0 +1,20 @@
+package edu.oakland.production.display01;
+
+import edu.oakland.helper.admin.Satellite;
+import edu.oakland.helper.display01.SatelliteSignalCheckRequest;
+
+public class DisplayGpsReceiverImplementation {
+    
+    public String measureGpsSignalStrength(Satellite satelliteSignal) {
+
+        return "";
+
+    }
+
+    public SatelliteSignalCheckRequest measureSignal(String satelliteName) {
+        
+        return null;
+        
+    }
+
+}
