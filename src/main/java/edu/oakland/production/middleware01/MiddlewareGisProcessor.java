3703624910996df28ffda49d42017880e461acf5
juliawang2014 <59949033+juliawang2014@users.noreply.github.com>
2020-11-24 12:43:12 -0500

    Update MiddlewareGisProcessor.java

--- src/main/java/edu/oakland/production/middleware01/MiddlewareGisProcessor.java
+++ src/main/java/edu/oakland/production/middleware01/MiddlewareGisProcessor.java
@@ -3,6 +3,9 @@ package edu.oakland.production.middleware01;
 import edu.oakland.helper.admin.Satellite;
 
 public interface MiddlewareGisProcessor {
-    public String evaluateGpsSignalStrength(Satellite satelliteSignal);
-    public String measureGpsSignalStrength(Satellite satelliteSignal);
+    
+  public String evaluateGpsSignalStrength(Satellite satelliteSignal);
+
+  public String measureGpsSignalStrength(Satellite satelliteSignal);
+
 }
