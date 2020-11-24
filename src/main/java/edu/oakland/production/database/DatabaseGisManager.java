c3eeb4d9ba2e3d985727dd9d5966e2d38e5fb105
robertsonbrinker <robertsonbrinker@gmail.com>
2020-11-24 14:49:43 -0500

    Update DatabseGisManager.java
    update to the interface

--- src/main/java/edu/oakland/production/database/DatabaseGisManager.java
+++ src/main/java/edu/oakland/production/database/DatabaseGisManager.java
@@ -4,8 +4,11 @@ import edu.oakland.helper.database;
 import edu.oakland.helper.LocationDataPoint;
 
 public interface DatabaseGisManager {
+	
   public void passStoreRequest(LocationDataPoint locationDataPoint);
   
   public void passModeRequest(String mode);
+  
   public String passNextSatRequest(String currentSat);
+  
 }
