532710d5127957d80db322b6936ae449fb1d53ca
juliawang2014 <59949033+juliawang2014@users.noreply.github.com>
2020-11-24 12:33:27 -0500

    [Middleware01] Create Public Interfaces
    Made the public interfaces #28
    
    Co-Authored-By: martinhughes0722 <60445081+martinhughes0722@users.noreply.github.com>
    Co-Authored-By: duongy18418 <55992627+duongy18418@users.noreply.github.com>
    Co-Authored-By: brendam19 <56038943+brendam19@users.noreply.github.com>
    Co-Authored-By: san4497 <56036629+san4497@users.noreply.github.com>
    Co-Authored-By: Slackjaw1431 <55801287+Slackjaw1431@users.noreply.github.com>

---
+++ src/main/java/edu/oakland/production/middleware01/MiddlewareGisInterface.java
@@ -0,0 +1,7 @@
+package edu.oakland.production.middleware01;
+
+import edu.oakland.helper.admin.Satellite;
+
+public interface MiddlewareGisInterface {
+    public String receiveGpsSignalStrength(Satellite satelliteSignal);
+}
