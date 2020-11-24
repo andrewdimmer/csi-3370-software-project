802af03fc6d1973b04b0a7c388dac20b79aa4cbd
juliawang2014 <59949033+juliawang2014@users.noreply.github.com>
2020-11-24 12:42:22 -0500

    Update MiddlewareGisManager.java

--- src/main/java/edu/oakland/production/middleware01/MiddlewareGisManager.java
+++ src/main/java/edu/oakland/production/middleware01/MiddlewareGisManager.java
@@ -3,8 +3,13 @@ package edu.oakland.production.middleware01;
 import edu.oakland.helper.admin.Satellite;
 
 public interface MiddlewareGisManager {
-    public String receiveGpsSignalStrength(Satellite satelliteSignal);
-    public void storeLocationDataPoint();
-    public String attemptToRegainGpsLock(String satelliteName);
-    public String evaluateGpsSignalStrength(boolean signalValid);
+    
+  public String receiveGpsSignalStrength(Satellite satelliteSignal);
+    
+  public void storeLocationDataPoint();
+    
+  public String attemptToRegainGpsLock(String satelliteName);
+    
+  public String evaluateGpsSignalStrength(boolean signalValid);
+
 }
