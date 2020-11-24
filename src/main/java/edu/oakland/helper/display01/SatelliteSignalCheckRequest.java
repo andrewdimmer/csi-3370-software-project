b9443a2e778ec9d9ca6e1207a5ab5ec1e0f66461
John-A11 <73508600+John-A11@users.noreply.github.com>
2020-11-23 21:44:38 -0500

    Update SatelliteSignalCheckRequest.java
    Adjusted spacing according to Google Java Style Guide.

--- src/main/java/edu/oakland/helper/display01/SatelliteSignalCheckRequest.java
+++ src/main/java/edu/oakland/helper/display01/SatelliteSignalCheckRequest.java
@@ -1,11 +1,11 @@
 package edu.oakland.helper.display01;
 
 public class SatelliteSignalCheckRequest {
-	
-    public String getSatelliteName() {
+
+  public String getSatelliteName() {
         return "";
     }
-	
+
     String getCheckType() {
         return "";
     }
