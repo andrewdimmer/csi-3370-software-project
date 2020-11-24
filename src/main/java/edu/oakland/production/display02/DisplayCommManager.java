91dc8682b2ad18b553260ba915dd6e1015fd8110
Jessica Cortes <jcortes@oakland.edu>
2020-11-24 02:20:22 -0500

    Update DisplayCommManager.java

--- src/main/java/edu/oakland/production/display02/DisplayCommManager.java
+++ src/main/java/edu/oakland/production/display02/DisplayCommManager.java
@@ -2,7 +2,7 @@ package edu.oakland.production.display02;
 
 import edu.oakland.helper.admin.TrackData;
 
-public interface DisplayCommInterface {
+public interface DisplayCommManager {
 
   public TrackData evaluateRfidRequest(int rfidNum);
 
