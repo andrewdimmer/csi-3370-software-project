ead64b829dfea1b844ebbb70d950d114b9e9799c
MathewYaldo <55512598+MathewYaldo@users.noreply.github.com>
2020-11-24 14:38:31 -0500

    Update DatabasePersistentStorage.java

--- src/main/java/edu/oakland/production/database/DatabasePersistentStorage.java
+++ src/main/java/edu/oakland/production/database/DatabasePersistentStorage.java
@@ -5,14 +5,22 @@ import edu.oakland.test.database;
 
 public interface DatabasePersistentStorage {
 
-	public int locateRfidData();
-	public LocationDataPoint getLocationDataPoint(int offset);
-	public void storeLocationDataPoint(LocationDataPoint locationDataPoint);
-	public TrackData getTrackData(int offset);
-	public void storeTrackData(TrackData trackData);
-	public String getMode();
+  public int locateRfidData();
 
-	public void storeMode(String mode);
-	public String getNextSat(String currentSat);
-	
+  public LocationDataPoint getLocationDataPoint(int offset);
+
+  public void storeLocationDataPoint(LocationDataPoint locationDataPoint);
+
+  public TrackData getTrackData(int offset);
+
+  public void storeTrackData(TrackData trackData);
+
+  public String getMode();
+
+  public void storeMode(String mode);
+  
+  public String getNextSat(String currentSat);
+
+
 }
+
