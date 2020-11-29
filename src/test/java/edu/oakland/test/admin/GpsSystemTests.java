72e7081b9c10dea856b3359d370afb5df120c2c0
brendanfraser597 <73411747+brendanfraser597@users.noreply.github.com>
2020-11-29 17:02:30 -0500

    Create GpsSystemTests.java

---
+++ src/test/java/edu/oakland/test/admin/GpsSystemTests.java
@@ -0,0 +1,14 @@
+package edu.oakland.test.admin;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.lang.IllegalArgumentException;
+import java.time.LocalDateTime;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("GpsSystem Unit Tests")
+public class GpsSystemTests {
+
+}
