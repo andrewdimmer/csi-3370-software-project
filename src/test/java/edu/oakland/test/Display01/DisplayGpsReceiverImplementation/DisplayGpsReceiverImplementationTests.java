61494faf857ceb7dc081b8c1492a088cf00b83ba
EC01EC <73436762+EC01EC@users.noreply.github.com>
2020-11-29 18:53:53 -0500

    Update DisplayGpsReceiverImplementationTests.java
    Changes to casting int to Integer and String.
    
    Implemented displayGpsManagerIsNotNull method

--- src/test/java/edu/oakland/test/Display01/DisplayGpsReceiverImplementation/DisplayGpsReceiverImplementationTests.java
+++ src/test/java/edu/oakland/test/Display01/DisplayGpsReceiverImplementation/DisplayGpsReceiverImplementationTests.java
@@ -1,12 +1,11 @@
 package edu.oakland.test.Display01.DisplayGpsReceiverImplementation;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import java.util.Random;
-import java.lang.NullPointerException;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import edu.oakland.helper.admin.Satellite;
+import edu.oakland.production.display01.DisplayGpsManagerImplementation;
 import edu.oakland.production.display01.DisplayGpsReceiverImplementation;
 
 @DisplayName ("Display GPS Receiver Implementation")
@@ -15,20 +14,26 @@ public class DisplayGpsReceiverImplementationTests {
   @DisplayName ("Measure GPS Signal Strength")
   public void measureGpsSignalStrength(Satellite satelliteSignal){
     Satellite signal = satelliteSignal;
-    int i = signal.getStrength();
+    Integer i = signal.getStrength();
+    String s = i.toString();
     if (i > 4){
-      DisplayGpsManagerImplementation manager = new DisplayGpsManagerImplementation(Satellite signal);
+      assertTrue(Integer.parseInt(s) > 4);
+      DisplayGpsManagerImplementation manager = new DisplayGpsManagerImplementation();
+      manager.passGpsSignalStrength(s);
     }
     else if(i <= 4){
-      displayGpsReceiverIsNotNull();
+      displayGpsManagerIsNotNull();
     }
     else{
-      break;
+      System.out.println("GPS Signal Strength Passed is Null. Exiting...");
+      System.exit(10);
     }
   }
+ 
   @Test
   @DisplayName ("Display GPS Manager Is Not Null")
-  public void displayGpsReceiverIsNotNull(){
-    
+  public void displayGpsManagerIsNotNull(){
+    DisplayGpsManagerImplementation manager = new DisplayGpsManagerImplementation();
+    assertNotNull(manager, "Verified that signal is not null");
   }
 }
