de2fec8026437cb73ec2ed2aa37f4ee4ac2b8b08
Andrew Dimmer <contactandrewd@gmail.com>
2020-11-28 22:46:13 -0500

    [Admin] Build TrackData

--- src/main/java/edu/oakland/helper/admin/TrackData.java
+++ src/main/java/edu/oakland/helper/admin/TrackData.java
@@ -4,20 +4,71 @@ import edu.oakland.helper.admin.LocationDataPoint;
 
 public class TrackData {
 
+  final LocationDataPoint[] points;
+  final float courseDirection;
+  final float speed;
+  final boolean validData;
+
+  public TrackData(LocationDataPoint[] points) {
+    // Validate points array
+    if (points == null) {
+      throw new IllegalArgumentException("points array cannot be null");
+    }
+    if (points.length == 5) {
+      throw new IllegalArgumentException("No valid data should not have a course and speed set");
+    }
+
+    this.points = points;
+    this.courseDirection = 0;
+    this.speed = 0;
+    this.validData = false;
+  }
+
+  public TrackData(LocationDataPoint[] points, float courseDirection, float speed) {
+    // Validate points array
+    if (points == null) {
+      throw new IllegalArgumentException("points array cannot be null");
+    }
+    if (points.length < 5) {
+      throw new IllegalArgumentException("Not enough points to create a valid TrackData object");
+    }
+    if (points.length > 5) {
+      throw new IllegalArgumentException("Somehow you have TOO much data for a TrackData object");
+    }
+
+    // Validate Speed
+    if (speed < 0) {
+      throw new IllegalArgumentException("speed cannot be negative");
+    }
+    
+    // Validate courseDirection
+    if (courseDirection < 0) {
+      throw new IllegalArgumentException("courseDirection cannot be negative");
+    }
+    if (points.length > 360) {
+      throw new IllegalArgumentException("courseDirection must be less than 360 degrees");
+    }
+
+    this.points = points;
+    this.courseDirection = courseDirection;
+    this.speed = speed;
+    this.validData = true;
+  }
+
   public boolean isValid() {
-    return false;
+    return validData;
   }
 
   public float getCourseDirection() {
-    return 0;
+    return courseDirection;
   }
 
   public float getSpeed() {
-    return 0;
+    return speed;
   }
 
   public LocationDataPoint[] getLocationDataPoints() {
-    return null;
+    return points;
   }
 
   public String getStatusMessage() {
