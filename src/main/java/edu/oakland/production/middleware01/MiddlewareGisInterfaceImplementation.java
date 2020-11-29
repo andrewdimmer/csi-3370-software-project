e01bb75b07a4873e1a71991ed044ea015ae8ff82
juliawang2014 <59949033+juliawang2014@users.noreply.github.com>
2020-11-29 15:48:34 -0500

    [Middleware_01] Added documentation to Implementation Classes

--- src/main/java/edu/oakland/production/middleware01/MiddlewareGisInterfaceImplementation.java
+++ src/main/java/edu/oakland/production/middleware01/MiddlewareGisInterfaceImplementation.java
@@ -3,10 +3,21 @@ package edu.oakland.production.middleware01;
 import edu.oakland.production.middleware01.MiddlewareGisInterface;
 import edu.oakland.helper.admin.Satellite;
 
+/**
+ * This class shall recieve that GPS signal from display and pass it to GIS processor.
+ *
+ */
+
 public class MiddlewareGisInterfaceImplementation implements MiddlewareGisInterface {
 
   private MiddlewareGisProcessor middlewareGisProcessor;
 
+  /**
+   * Creates a MiddlewareGisInterfaceImplementation to receive the Satellite signal.
+   *
+   * @param middlewareGisProcessor  The class that this class will pass the Satellite signal to.
+   *
+   */
   public MiddlewareGisInterfaceImplementation(MiddlewareGisProcessor middlewareGisProcessor){
     this.middlewareGisProcessor = middlewareGisProcessor;
     if (this.middlewareGisProcessor == null) {
@@ -14,10 +25,20 @@ public class MiddlewareGisInterfaceImplementation implements MiddlewareGisInterf
     }
   }
 
+  /**
+   * Receives the Signal Strength for Use Case 1.
+   *
+   * @return The Signal Strength.
+   */
   public String receiveGpsSignalStrengthUc1(Satellite satelliteSignal) {
     return middlewareGisProcessor.evaluateGpsSignalStrength(satelliteSignal);
   }
 
+  /**
+   * Receives the Signal Strength for Use Case 2.
+   *
+   * @return The Signal Strength.
+   */
   public String receiveGpsSignalStrengthUc2(Satellite satelliteSignal) {
     return middlewareGisProcessor.measureGpsSignalStrength(satelliteSignal);
   }
