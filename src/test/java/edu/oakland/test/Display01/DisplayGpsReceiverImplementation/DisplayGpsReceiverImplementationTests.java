6c37360d2af5d247f7bc143d8f74048efe761c00
EC01EC <73436762+EC01EC@users.noreply.github.com>
2020-11-29 18:27:24 -0500

    Update DisplayGpsReceiverImplementationTests.java
    Style guide fixes?

--- src/test/java/edu/oakland/test/Display01/DisplayGpsReceiverImplementation/DisplayGpsReceiverImplementationTests.java
+++ src/test/java/edu/oakland/test/Display01/DisplayGpsReceiverImplementation/DisplayGpsReceiverImplementationTests.java
@@ -11,24 +11,24 @@ import edu.oakland.production.display01.DisplayGpsReceiverImplementation;
 
 @DisplayName ("Display GPS Receiver Implementation")
 public class DisplayGpsReceiverImplementationTests {
-    @Test
-    @DisplayName ("Measure GPS Signal Strength")
-    public void measureGpsSignalStrength(Satellite satelliteSignal){
-        Satellite signal = satelliteSignal;
-        int i = signal.getStrength();
-        if (i > 4){
-            DisplayGpsManagerImplementation manager = new DisplayGpsManagerImplementation(Satellite signal);
-        }
-        else if(i <= 4){
-            displayGpsReceiverIsNotNull();
-        }
-        else{
-            break;
-        }
+  @Test
+  @DisplayName ("Measure GPS Signal Strength")
+  public void measureGpsSignalStrength(Satellite satelliteSignal){
+    Satellite signal = satelliteSignal;
+    int i = signal.getStrength();
+    if (i > 4){
+      DisplayGpsManagerImplementation manager = new DisplayGpsManagerImplementation(Satellite signal);
     }
-    @Test
-    @DisplayName ("Display GPS Manager Is Not Null")
-    public void displayGpsReceiverIsNotNull(){
-
+    else if(i <= 4){
+      displayGpsReceiverIsNotNull();
     }
+    else{
+      break;
+    }
+  }
+  @Test
+  @DisplayName ("Display GPS Manager Is Not Null")
+  public void displayGpsReceiverIsNotNull(){
+    
+  }
 }
