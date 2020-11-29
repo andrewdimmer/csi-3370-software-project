89e2aaafe98a804badd1e2ccc0570eeaafe9bcdf
EC01EC <73436762+EC01EC@users.noreply.github.com>
2020-11-29 18:17:10 -0500

    Update DisplayGpsReceiverImplementationTests.java
    Implemented measureGpsSignalStrength Method. Working on displayGpsManagerIsNotNull Method next.

--- src/test/java/edu/oakland/test/Display01/DisplayGpsReceiverImplementation/DisplayGpsReceiverImplementationTests.java
+++ src/test/java/edu/oakland/test/Display01/DisplayGpsReceiverImplementation/DisplayGpsReceiverImplementationTests.java
@@ -1,5 +1,35 @@
-package edu.oakland.test.Display01.DisplayGpsReceiverImplementationTests;
+package edu.oakland.test.Display01.DisplayGpsReceiverImplementation;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import java.util.Random;
+import java.lang.NullPointerException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import edu.oakland.helper.admin.Satellite;
+import edu.oakland.production.display01.DisplayGpsReceiverImplementation;
+
+@DisplayName ("Display GPS Receiver Implementation")
 public class DisplayGpsReceiverImplementationTests {
-    
+    @Test
+    @DisplayName ("Measure GPS Signal Strength")
+    public void measureGpsSignalStrength(Satellite satelliteSignal){
+        Satellite signal = satelliteSignal;
+        int i = ((Integer)signal).intValue();
+        if (i > 4){
+            DisplayGpsManagerImplementation manager = new DisplayGpsManagerImplementation(Satellite signal){
+            }
+        }
+        else if(i <= 4){
+            displayGpsReceiverIsNotNull();
+        }
+        else{
+            break;
+        }
+    }
+    @Test
+    @DisplayName ("Display GPS Manager Is Not Null")
+    public void displayGpsReceiverIsNotNull(){
+
+    }
 }
