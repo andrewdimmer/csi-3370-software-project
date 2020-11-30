d26f95d6f5a763e2cfb3247bec81801b716ed0b0
Robertson Brinker <60048070+robertsonbrinker@users.noreply.github.com>
2020-11-29 21:36:18 -0500

    Update DatabaseGisManagerStub.java

--- src/main/java/edu/oakland/production/database/DatabaseGisManagerStub.java
+++ src/main/java/edu/oakland/production/database/DatabaseGisManagerStub.java
@@ -4,16 +4,21 @@ import edu.oakland.helper.admin.LocationDataPoint;
 
 public class DatabaseGisManagerStub implements DatabaseGisManager {
 
+  private LocationDataPoint ldp;
+
   public String passNextSatRequest(String currentSat) {
     return currentSat;
   }
 
   public LocationDataPoint getLocationDataPoint(int offset) {
-    return ldp;
+    if (offset = 0) {
+      return ldp;
+    }
+    return null;
   }
 
   public void passStoreRequest(LocationDataPoint locationDataPoint) {
-    
+    ldp = locationDataPoint;
   }
 
   public String passModeRequest(String mode) {
