46fe4640955d128ca7e4c694a5715d7d2a52efc2
Endrit <endritzenuni112@gmail.com>
2020-11-30 14:29:03 -0500

    Update DatabaseCommManagerImplementationTest.java

--- src/test/java/edu/oakland/test/database/DatabaseCommManagerImplementationTest.java
+++ src/test/java/edu/oakland/test/database/DatabaseCommManagerImplementationTest.java
@@ -21,8 +21,7 @@ public class DatabaseCommManagerImplementationTest {
 
     int i = (int) (Math.random() * 100);
 
-    TrackData td = new TrackData(generateRandomLocationDataPointsArray(10));
-    DatabasePersistentStorageStub dpss = new DatabasePersistentStorageStub(i, td);
+    DatabasePersistentStorageStub dpss = new DatabasePersistentStorageStub(i);
     DatabaseCommManager dcm = new DatabaseCommManagerImplementation(dpss);
     assertEquals(i, dcm.passGetRfidRequest());
   }
@@ -31,16 +30,19 @@ public class DatabaseCommManagerImplementationTest {
   @DisplayName("Offset matches Datapoint")
   void offsetMatchDataPoint() {
     int i = (int) (Math.random() * 100);
-    TrackData td = new TrackData(generateRandomLocationDataPointsArray(10));
-    DatabasePersistentStorageStub dpss = new DatabasePersistentStorageStub(i, td);
+
+    DatabasePersistentStorageStub dpss = new DatabasePersistentStorageStub(i);
     DatabaseCommManager dcm = new DatabaseCommManagerImplementation(dpss);
 
     float lat = (float) (Math.random() * 360 - 180);
     float lng = (float) (Math.random() * 360 - 180);
 
     LocationDataPoint ldp = new LocationDataPoint(lat, lng,
-        LocalDateTime.of((int) (Math.random() * 50 + 1970), (int) (Math.random() * 12 + 1),
-            (int) (Math.random() * 28 + 1), (int) (Math.random() * 24), (int) (Math.random() * 60)));
+        LocalDateTime.of((int) (Math.random() * 50 + 1970),
+         (int) (Math.random() * 12 + 1),
+            (int) (Math.random() * 28 + 1),
+             (int) (Math.random() * 24),
+              (int) (Math.random() * 60)));
     dpss.storeLocationDataPoint(ldp);
     assertEquals(ldp, dcm.passGetLocationDataPointRequest(i));
   }
@@ -49,8 +51,8 @@ public class DatabaseCommManagerImplementationTest {
   @DisplayName("Offset matches TrackData")
   void offsetMatchTrackData() {
     int i = (int) (Math.random() * 100);
-    TrackData td = new TrackData(generateRandomLocationDataPointsArray(10));
-    DatabasePersistentStorageStub dpss = new DatabasePersistentStorageStub(i, td);
+
+    DatabasePersistentStorageStub dpss = new DatabasePersistentStorageStub(i);
     DatabaseCommManager dcm = new DatabaseCommManagerImplementation(dpss);
     int j = (int) (Math.random() * 100);
 
@@ -87,8 +89,11 @@ public class DatabaseCommManagerImplementationTest {
   }
 
   private LocationDataPoint generateRandomLocationDataPoint() {
-    return new LocationDataPoint((float) (Math.random() * 180 - 90), (float) (Math.random() * 360 - 180),
-        LocalDateTime.of((int) (Math.random() * 50 + 1970), (int) (Math.random() * 12 + 1),
-            (int) (Math.random() * 28 + 1), (int) (Math.random() * 24), (int) (Math.random() * 60)));
+    return new LocationDataPoint((float) (Math.random() * 180 - 90),
+     (float) (Math.random() * 360 - 180),
+        LocalDateTime.of((int) (Math.random() * 50 + 1970),
+         (int) (Math.random() * 12 + 1),
+            (int) (Math.random() * 28 + 1), (int) (Math.random() * 24),
+             (int) (Math.random() * 60)));
   }
 }
