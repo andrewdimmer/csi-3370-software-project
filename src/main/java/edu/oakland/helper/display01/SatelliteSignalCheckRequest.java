e41c1fdea0eff4f508abc1625eeb6032ab976572
John-A11 <73508600+John-A11@users.noreply.github.com>
2020-11-29 23:31:43 -0500

    [Display_01] Built SatelliteSignalCheckRequest

--- src/main/java/edu/oakland/helper/display01/SatelliteSignalCheckRequest.java
+++ src/main/java/edu/oakland/helper/display01/SatelliteSignalCheckRequest.java
@@ -1,12 +1,64 @@
 package edu.oakland.helper.display01;
 
+import java.lang.IllegalArgumentException;
+
+/**
+ * A common helper class to check name and type of the Satellite Signal for the Secure Lock Tracking Software
+ * shipping container.
+ *
+ * @author John Akroush, Steven Catherman
+ */
+
 public class SatelliteSignalCheckRequest {
 
-  public String getSatelliteName() {
-    return "";
+  final String satelliteName;
+  final String checkType;
+
+/**
+   * Creates a SatelliteSignalCheckRequest to check the satellite name and type of the Satellite Signal for Secure Lock Tracking
+   * Software shipping container.
+   *
+   * @param satelliteName  The name of the satellite of the Secure Lock Tracking Software shipping container.
+   * @param checkType  The type of the Secure Lock Tracking Software shipping container.
+   */
+
+  public SatelliteSignalCheckRequest(String satelliteName, String checkType) {
+    //Validate name and type
+    if (satelliteName == null) {
+      throw new IllegalArgumentException("name cannot be null");
+    }
+
+    if (checkType == null) {
+      throw new IllegalArgumentException("type cannot be null");
+    }
+
+    if (satelliteName.length() == 0) {
+      throw new IllegalArgumentException("sattellite name cannot be empty");
+    }
+    if (checkType.length() == 0) {
+      throw new IllegalArgumentException("check type cannot be empty");
+    }
+    
+    
+    this.satelliteName = satelliteName;
+    this.checkType = checkType;
   }
 
+  /**
+   * Gets the Name of the satellite of the Secure Lock Tracking Software shipping container.
+   *
+   * @return The name of the satellite of the Secure Lock Tracking Software shipping container.
+   */
+  public String getSatelliteName() {
+    return satelliteName;
+  }
+
+  /**
+   * Gets the type of the satellite of the Secure Lock Tracking Software shipping container.
+   *
+   * @return The type of the satellite of the Secure Lock Tracking Software shipping container.
+   */
   public String getCheckType() {
-    return "";
+    return checkType;
   }
 }
