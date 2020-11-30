76d73edc646a6c1de844cb4cca281f32285a85c2
EC01EC <73436762+EC01EC@users.noreply.github.com>
2020-11-30 00:57:44 -0500

    Update DisplayGpsReceiverImplementationTests.java
    Major changes to displayGpsManagerIsNotNull:
    
    - moved null check to ../src/main/java/edu/oakland/production/display01/DisplayGpsReceiverImplementation.java
    
    - displayGpsManagerIsNotNull now checks for throw of IllegalArgumentException

--- src/test/java/edu/oakland/test/Display01/DisplayGpsReceiverImplementation/DisplayGpsReceiverImplementationTests.java
+++ src/test/java/edu/oakland/test/Display01/DisplayGpsReceiverImplementation/DisplayGpsReceiverImplementationTests.java
@@ -1,9 +1,8 @@
 package edu.oakland.test.Display01.DisplayGpsReceiverImplementation;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import edu.oakland.helper.admin.Satellite;
-import edu.oakland.production.display01.DisplayGpsManagerImplementation;
+import edu.oakland.production.display01.DisplayGpsReceiverImplementation;
 import java.lang.IllegalArgumentException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -14,14 +13,9 @@ import org.junit.jupiter.api.Test;
 public class DisplayGpsReceiverImplementationTests {
   @Test
   @DisplayName ("Display GPS Manager Is Not Null")
-  public void displayGpsManagerIsNotNull(Satellite satelliteSignal) {
-    Integer i = satelliteSignal.getStrength();
-    String s = i.toString();
-    if (s != null) {
-      DisplayGpsManagerImplementation manager = new DisplayGpsManagerImplementation();
-      assertNotNull(manager, "Verified that signal is not null");
-    } else {
-      throw new IllegalArgumentException("Signal strength cannot be null");
-    }
+  public void displayGpsManagerIsNotNull() {
+    assertThrows(IllegalArgumentException.class, () -> {
+      new measureGpsSignalStrength(null);
+    });
   }
 }
