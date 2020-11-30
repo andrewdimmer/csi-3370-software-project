da2e67f6267f0ecbf11eeb76aacf325aaf3a4772
NMoua-Edu <73411759+NMoua-Edu@users.noreply.github.com>
2020-11-30 17:37:58 -0500

    Added Javadoc Comments

--- src/main/java/edu/oakland/production/middleware02/MiddlewareCommInterfaceImplementation.java
+++ src/main/java/edu/oakland/production/middleware02/MiddlewareCommInterfaceImplementation.java
@@ -2,11 +2,20 @@ package edu.oakland.production.middleware02;
 
 import edu.oakland.helper.admin.TrackData;
 import edu.oakland.production.middleware02.MiddlewareCommInterface;
-
+/** 
+* This class shall recieve the RFID from display and pass it to MiddlwareCommLinkManager.
+*
+*/
 public class MiddlewareCommInterfaceImplementation implements MiddlewareCommInterface {
   int rfid;
   
   private MiddlewareCommLinkManager middlewareManager;
+  /** 
+   * Creates a MiddlewareCommInterfaceImplementation to recive the Rfid.
+   * 
+   * @param middlewareManager the class that this class will pass the Rfid to.
+   * 
+  */
 
   public MiddlewareCommInterfaceImplementation(MiddlewareCommLinkManager middlewareManager) {
     if(middlewareManager == null) {
@@ -14,6 +23,12 @@ public class MiddlewareCommInterfaceImplementation implements MiddlewareCommInte
     }
     this.middlewareManager = middlewareManager;
   }
+
+  /**
+   * Receives the rfid for Use Case 1.
+   * 
+   * @return rfid.
+   */
 
   public TrackData requestRfid(int rfid) {
     return middlewareManager.checkCurrentRfid(rfid);
--- src/test/java/edu/oakland/test/middleware02/MiddlewareCommLinkManagerStub.java
+++ src/test/java/edu/oakland/test/middleware02/MiddlewareCommLinkManagerStub.java
@@ -3,7 +3,9 @@ package edu.oakland.test.middleware02;
 import edu.oakland.helper.admin.LocationDataPoint;
 import edu.oakland.helper.admin.TrackData;
 import edu.oakland.production.middleware02.MiddlewareCommLinkManager;
-
+/**
+ * This class shall get the current rfid and set it in trackdata.
+ */
 
 public class MiddlewareCommLinkManagerStub implements MiddlewareCommLinkManager {
   public TrackData checkCurrentRfid(int rfid) {
