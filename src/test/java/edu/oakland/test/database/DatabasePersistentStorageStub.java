6e1e1bcb27afd9fcb5c844d8c5ce85136b36ebe0
Jake Gleason <jrgleason@oakland.edu>
2020-11-30 13:51:27 -0500

    [Database] Build Database GISManager (#139)
    * Database GIS Manger Stuff
    
    * Update DatabasePersistentStorageStub.java
    
    * Update DatabasePersistentStorageStub.java
    
    * Update DatabasePersistentStorageStub.java
    
    * Update DatabasePersistentStorageStub.java
    
    * Update DatabasePersistentStorageStub.java
    
    * Update DatabaseGisManagerTest.java
    
    * Update DatabaseGisManagerTest.java
    
    * Update src/main/java/edu/oakland/production/database/DatabaseGisManagerImplementation.java
    
    Co-authored-by: Andrew Dimmer <43498863+andrewdimmer@users.noreply.github.com>
    
    * Update src/test/java/edu/oakland/test/database/DatabaseGisManagerTest.java
    
    Co-authored-by: Andrew Dimmer <43498863+andrewdimmer@users.noreply.github.com>
    
    * Update DatabaseGisManagerTest.java
    
    Co-authored-by: Andrew Dimmer <43498863+andrewdimmer@users.noreply.github.com>
    Co-authored-by: Camron Grant <56035567+camrongrant1@users.noreply.github.com>

---
+++ src/test/java/edu/oakland/test/database/DatabasePersistentStorageStub.java
@@ -0,0 +1,46 @@
+package edu.oakland.test.database;
+
+import edu.oakland.helper.admin.LocationDataPoint;
+import edu.oakland.helper.admin.TrackData;
+import edu.oakland.production.database.DatabasePersistentStorage;
+
+public class DatabasePersistentStorageStub implements DatabasePersistentStorage {
+
+  private LocationDataPoint ldp;
+  private String mode;
+
+  public String getNextSat(String currentSat) {
+    return currentSat;
+  }
+
+  public LocationDataPoint getLocationDataPoint(int offset) {
+    return ldp;
+  }
+
+  public void storeLocationDataPoint(LocationDataPoint locationDataPoint) {
+    ldp = locationDataPoint;
+  }
+
+  public void storeMode(String mode) {
+    this.mode = mode;
+  }
+
+  public String getMode() {
+    return mode;
+  }
+  
+  public TrackData getTrackData(int offset) {
+    TrackData td = null;
+    return td;
+  }
+  
+  public int locateRfidData() {
+    return 10;
+  }
+
+  public void storeTrackData(TrackData trackData) {
+    
+  }
+  
+ 
+}
