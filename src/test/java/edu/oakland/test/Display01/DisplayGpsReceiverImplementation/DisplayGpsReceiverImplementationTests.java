955e3d9c1b2046cbda38dc34379007f0642ba513
EC01EC <73436762+EC01EC@users.noreply.github.com>
2020-11-30 00:11:51 -0500

    Update DisplayGpsReceiverImplementationTests.java
    Attempts to fix build issues. Not resolved

--- src/test/java/edu/oakland/test/Display01/DisplayGpsReceiverImplementation/DisplayGpsReceiverImplementationTests.java
+++ src/test/java/edu/oakland/test/Display01/DisplayGpsReceiverImplementation/DisplayGpsReceiverImplementationTests.java
@@ -1,10 +1,9 @@
-package edu.oakland.test.display01.DisplayGpsReceiverImplementation;
+package edu.oakland.test.Display01.DisplayGpsReceiverImplementation;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import edu.oakland.helper.admin.Satellite;
 import edu.oakland.production.display01.DisplayGpsManagerImplementation;
-import edu.oakland.production.display01.DisplayGpsReceiverImplementation;
 import java.lang.IllegalArgumentException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -16,8 +15,7 @@ public class DisplayGpsReceiverImplementationTests {
   @Test
   @DisplayName ("Display GPS Manager Is Not Null")
   public void displayGpsManagerIsNotNull(Satellite satelliteSignal) {
-    Satellite signal = satelliteSignal;
-    Integer i = signal.getStrength();
+    Integer i = satelliteSignal.getStrength();
     String s = i.toString();
     if (s != null) {
       DisplayGpsManagerImplementation manager = new DisplayGpsManagerImplementation();
