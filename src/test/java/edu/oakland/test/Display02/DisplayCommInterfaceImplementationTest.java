89c9c3e0afb078200bfe5c21654c8682eaa18fad
Jessica Cortes <jcortes@oakland.edu>
2020-11-30 13:24:27 -0500

    Updated DisplaycCommInterface Implementation and Tests

--- src/test/java/edu/oakland/test/Display02/DisplayCommInterfaceImplementationTest.java
+++ src/test/java/edu/oakland/test/Display02/DisplayCommInterfaceImplementationTest.java
@@ -1,33 +1,31 @@
-package edu.oakland.test.Display02;
+package edu.oakland.test.display02;
 
-import edu.oakland.production.display02.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import edu.oakland.production.display02.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-@DisplayName("Display Comm Interface Implementation Tests")
+@DisplayName("DisplayCommInterface Implementation Tests")
 public class DisplayCommInterfaceImplementationTest {
-    
-    @Test
 
-    void rfidInIsRfidOut(){
-        int rfid = generateRandomRfid();
-        DisplayCommInterfaceImplementation displayComm = new DisplayCommInterfaceImplementation();
-        displayComm.receiveRfidRequest(rfid);
-        int output = displayComm.rfid;
-        assertEquals(rfid, output);
-        }
+  @Test
+  void rfidInIsRfidOut() {
+    int rfid = generateRandomRfid();
+    DisplayCommInterfaceImplementation displayComm = new DisplayCommInterfaceImplementation();
+    displayComm.receiveRfidRequest(rfid);
+    int output = displayComm.rfid;
+    assertEquals(rfid, output);
+  }
 
-    @Test
-    void displayComm2WayIsNotNull(){
-        DisplayComm2WayInterfaceImplementation DC = new DisplayComm2WayInterfaceImplementation();
-        assertNotNull(DC, "Verify that class is not null");
-    }
-    private int generateRandomRfid() {
-        return (int) Math.random();
-      }
+  @Test
+  void displayComm2WayIsNotNull() {
+    DisplayComm2WayInterfaceImplementation dc = new DisplayComm2WayInterfaceImplementation();
+    assertNotNull(dc, "Verify that class is not null");
+  }
+  private int generateRandomRfid() {
+
+    return (int) Math.random();
+  }
 }
-
-  
