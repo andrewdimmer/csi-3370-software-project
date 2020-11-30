2289fbe0fb69606d4add9b7eb11010de8673417f
jmccloud827 <60444521+jmccloud827@users.noreply.github.com>
2020-11-30 01:15:53 -0500

    Update DisplayComm2WayInterfaceImplementationTests.java

--- src/test/java/edu/oakland/test/display02/DisplayComm2WayInterfaceImplementationTests.java
+++ src/test/java/edu/oakland/test/display02/DisplayComm2WayInterfaceImplementationTests.java
@@ -1,6 +1,8 @@
-package edu.oakland.test.Display02;
+package edu.oakland.test.display02;
 
-import edu.oakland.production.display02.*;
+import edu.oakland.production.display02.DisplayComm2WayInterface;
+import edu.oakland.production.display02.DisplayCommManager;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
@@ -9,16 +11,16 @@ import org.junit.jupiter.api.Test;
 
 @DisplayName("Display Comm 2 Way Interface Implementation Tests")
 public class DisplayComm2WayInterfaceImplementationTests {
-    @Test
-    void rfidInIsRfidOut(){
-        int rfid = 1101;//rfid number?
-        DisplayComm2WayInterfaceImplementation comm2Way = new DisplayComm2WayInterfaceImplementation();
-        int out = comm2Way.rfid;
-        assertEquals(rfid, out);
-        }
-    @Test
-    void displayComm2WayIsNotNull(){
-        DisplayCommManager manager = new DisplayCommManager();
-        assertNotNull(manager, "Checking if class is null");
-    }
+  @Test
+  void rfidInIsRfidOut(){
+    int rfid = 1101;//rfid number?
+    DisplayComm2WayInterfaceImplementation comm2Way = new DisplayComm2WayInterfaceImplementation();
+    int out = comm2Way.rfid;
+    assertEquals(rfid, out);
+  }
+  @Test
+  void displayComm2WayIsNotNull(){
+    DisplayCommManager manager = new DisplayCommManager();
+    assertNotNull(manager, "Checking if class is null");
+  }
 }
