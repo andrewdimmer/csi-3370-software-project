1e9482b2f02e31a7b27b600f78ac7424bfcd727a
jmccloud827 <60444521+jmccloud827@users.noreply.github.com>
2020-11-30 01:19:06 -0500

    Update DisplayComm2WayInterfaceImplementationTests.java

--- src/test/java/edu/oakland/test/display02/DisplayComm2WayInterfaceImplementationTests.java
+++ src/test/java/edu/oakland/test/display02/DisplayComm2WayInterfaceImplementationTests.java
@@ -13,11 +13,12 @@ import org.junit.jupiter.api.Test;
 public class DisplayComm2WayInterfaceImplementationTests {
   @Test
   void rfidInIsRfidOut(){
-    int rfid = 1101;//rfid number?
+    int rfid = 1101; //rfid number?
     DisplayComm2WayInterfaceImplementation comm2Way = new DisplayComm2WayInterfaceImplementation();
     int out = comm2Way.rfid;
     assertEquals(rfid, out);
   }
+
   @Test
   void displayComm2WayIsNotNull(){
     DisplayCommManager manager = new DisplayCommManager();
