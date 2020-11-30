50693bfe1d2e178b21d593a05af74672547ca19e
David Baker <davidbaker@oakland.edu>
2020-11-29 21:15:46 -0500

    Display CommInterface Test and IMP

---
+++ src/main/java/edu/oakland/production/display02/DisplayCommInterfaceImplementation.java
@@ -0,0 +1,13 @@
+package edu.oakland.production.display02;
+import edu.oakland.helper.admin.TrackData;
+
+
+public class DisplayCommInterfaceImplementation implements DisplayCommInterface{
+    public int rfid;
+    DisplayComm2WayInterfaceImplementation instance = new DisplayComm2WayInterfaceImplementation();
+    public TrackData receiveRfidRequest(int rfidNum){
+        rfid = rfidNum;
+        TrackData trackData = instance.passRfidRequest(rfidNum);
+        return trackData;
+    }
+}
