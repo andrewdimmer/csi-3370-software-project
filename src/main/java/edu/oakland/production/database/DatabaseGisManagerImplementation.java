fa21f18d60397de5929f6de4b6879c11a0ba6aae
Jake Gleason <jrgleason@oakland.edu>
2020-11-29 23:55:10 -0500

    Database GIS Manger Stuff

---
+++ src/main/java/edu/oakland/production/database/DatabaseGisManagerImplementation.java
@@ -0,0 +1,33 @@
+package edu.oakland.production.database;
+
+import edu.oakland.helper.admin.LocationDataPoint;
+
+public class DatabaseGisManagerImplementation {
+
+  DatabasePersistentStorage storage;
+
+  /**
+   * Creates a database manager that links the interface and storage.
+   *
+   * @param dps an instance of the DatabasePersistentStorage class.
+   */
+  public DatabaseGisManagerImplementation(DatabasePersistentStorage dps) {
+    if (dps == null) {
+      throw new IllegalArgumentException("storage cannot be null");
+    }
+    storage = dps;
+  }
+
+  public void passStoreRequest(LocationDataPoint locationDataPoint) {
+    storage.storeLocationDataPoint(locationDataPoint);
+  }
+
+  public void passModeRequest(String mode) {
+    storage.storeMode(mode);
+  } 
+
+  public String passNextSatRequest(String currentSat) {
+    return storage.getNextSat(currentSat);
+  }
+
+}
---
+++ src/test/java/edu/oakland/test/database/DatabasePersistentStorageStub.java
@@ -0,0 +1,30 @@
+package edu.oakland.test.database;
+
+import edu.oakland.helper.admin.LocationDataPoint;
+import edu.oakland.production.database.DatabasePersistentStorage;
+
+public class DatabasePersistentStorageStub implements DatabasePersistentStorage {
+
+  private LocationDataPoint ldp;
+  private String mode;
+
+  public String getNextSat(String currentSat) {
+    return currentSat;
+  }
+
+  public LocationDataPoint getLocationDataPoint() {
+    return ldp;
+  }
+
+  public void storeLocationDataPoint(LocationDataPoint locationDataPoint) {
+    ldp = locationDataPoint;
+  }
+
+  public void storeMode(String mode) {
+    this.mode = mode;
+  }
+
+  public String getMode() {
+    return mode;
+  }
+}
