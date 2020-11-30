60b889a973aa4b3c153a3274ff7bb0061c87f59b
Ahartman3 <43781478+Ahartman3@users.noreply.github.com>
2020-11-30 13:49:50 -0500

    Update DisplayCommManagerImplementation.java

--- src/main/java/edu/oakland/production/display02/DisplayCommManagerImplementation.java
+++ src/main/java/edu/oakland/production/display02/DisplayCommManagerImplementation.java
@@ -6,11 +6,22 @@ import edu.oakland.production.display02.DisplayCommManager;
 import edu.oakland.production.middleware02.MiddlewareCommInterface;
 import java.lang.IllegalArgumentException;
 
+/**
+* This class receives the rfid, evaluates it,
+* then passes it along to Middleware
+*
+*/
 public class DisplayCommManagerImplementation implements DisplayCommManager {
 
     private MiddlewareCommInterface middleware;
     public int rfid;
     
+    /**
+    * Creates a DisplayCommManagerImplementation to receive the rfid.
+    *
+    * @param MiddlewareCommInterface: The class that DisplayCommManager will pass the rfid to.
+    *
+    */
     public DisplayCommManagerImplementation(MiddlewareCommInterface middlewareCommInterface) {
         if (middlewareCommInterface == null) {
             throw new IllegalArgumentException("processor cannot be null");
@@ -18,15 +29,31 @@ public class DisplayCommManagerImplementation implements DisplayCommManager {
         this.middleware = middlewareCommInterface;
     }
 
+    /**
+    * Receives the rfid from DisplayComm2WayInterface
+    * before passing along for evaluation.
+    *
+    * @return the TrackData
+    */
     public TrackData receiveRfid(int rfidNum) {
         rfid = rfidNum;
         return evaluateRfidRequest(rfid);
     }
 
+    /**
+    * Evaluates the rfid received.
+    *
+    * @return the TrackData
+    */
     public TrackData evaluateRfidRequest(int rfid) {
         return passRfidToMiddlewareCommInterface(rfid);
     }
 
+    /**
+    * Passes the evaluated rfid along to MiddlewareCommInterface.
+    *
+    * @return the TrackData
+    */
     public TrackData passRfidToMiddlewareCommInterface(int rfid) {
         if (middleware == null) {
             throw new IllegalArgumentException("MiddlewareCommInterface cannot be null");
