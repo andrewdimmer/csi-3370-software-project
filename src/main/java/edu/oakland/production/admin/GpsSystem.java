2bc5a9f4c4e6be74912c80d77b76bd5d113e2a5c
brendanfraser597 <73411747+brendanfraser597@users.noreply.github.com>
2020-11-30 03:21:41 -0500

    make more tests and write more of GpsSystem

--- src/main/java/edu/oakland/production/admin/GpsSystem.java
+++ src/main/java/edu/oakland/production/admin/GpsSystem.java
@@ -2,6 +2,8 @@ package edu.oakland.production.admin;
 
 import java.lang.IllegalArgumentException;
 import java.time.LocalDateTime;
+import java.util.Random;
+import java.util.Scanner;
 
 import edu.oakland.production.display01.DisplayGpsInterface;
 import edu.oakland.helper.admin.Satellite;
@@ -16,8 +18,8 @@ import edu.oakland.helper.admin.LocationDataPoint;
  */
 public class GpsSystem {
 
-  final DisplayGpsInterface displayGpsInterface;
-  final Satellite[] satellites;
+  private final DisplayGpsInterface displayGpsInterface;
+  private final Satellite[] satellites;
 
   /**
    * Creates a GpsSystem object to store the GpsInterface and satellites.
@@ -27,11 +29,59 @@ public class GpsSystem {
    *
    */
   public GpsSystem(DisplayGpsInterface displayGpsInterfaceIn, String[] satelliteNames) {
-	  satellites = new Satellite[10];
-	  if (displayGpsInterfaceIn == null) {
+    if (displayGpsInterfaceIn == null) {  //3 checks to make sure the data that was passed in is valid
 		  throw new IllegalArgumentException("displayGpsInterface must not be null");
+	  } else if (satelliteNames == null) {
+      throw new IllegalArgumentException("satelliteNames must not be null");
+	  } else if (isEmptyStringInArray(satelliteNames)) {
+		  throw new IllegalArgumentException("satelliteNames must not be null");
 	  }
+	  satellites = new Satellite[satelliteNames.length];
 	  displayGpsInterface = displayGpsInterfaceIn;
   }
-
+  
+  /**
+   * Method to run UseCase 1, using supplied Scanner
+   *
+   * @param input   The scanner object we are passing in.
+   *
+   */
+  public void runUseCase1(Scanner input) {
+    if (input == null) { //check for null unput
+      throw new IllegalArgumentException("Scanner input must not be null");
+    } 
+    if (!this.displayGpsInterface.receiveGpsSignal(this.satellites[0])) {
+      runUseCase2(input);
+    } 
+  }
+      
+  /**
+   * Method to run UseCase 2, using supplied Scanner
+   *
+   * @param input   The scanner object we are passing in.
+   *
+   */
+  public void runUseCase2(Scanner input) {
+    if (input == null) { //check for null unput
+      throw new IllegalArgumentException("Scanner input must not be null");
+    }
+  }
+  
+  public void configureSatellites(String[] satelliteNames, float incrementLatAmount, float incrementLngAmount, LocationDataPoint initLocationDataPoint) {
+  }
+  
+  /**
+   * Checks if all strings in input array are of length > 0.
+   *
+   * @param in  The inputted array of strings.
+   *
+   */
+  private boolean isEmptyStringInArray(String[] in) {
+    for (int i = 0; i < in.length; i++) {
+      if (in[i].length() == 0) {
+        return true;
+      }
+    }
+    return false;
+  }
 }
--- src/test/java/edu/oakland/test/admin/GpsSystemTests.java
+++ src/test/java/edu/oakland/test/admin/GpsSystemTests.java
@@ -2,25 +2,129 @@ package edu.oakland.test.admin;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import java.lang.IllegalArgumentException;
 import java.time.LocalDateTime;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import java.util.Random;
+import java.util.Scanner;
 
 import edu.oakland.production.admin.GpsSystem;
+import edu.oakland.helper.admin.LocationDataPoint;
 import edu.oakland.test.admin.DisplayGpsInterfaceStub;
 
 @DisplayName("GpsSystem Unit Tests")
 public class GpsSystemTests {
 
 	@Test
-	@DisplayName("Init DisplayGpsInterfaceNotNull")
+	@DisplayName("Init Display GpsInterface Not Null")
 	void initDisplayGpsInterfaceNotNull(){
-	String[] str =  new String[]{"1","2","3"};
-	DisplayGpsInterfaceStub stub = new DisplayGpsInterfaceStub();
-	assertThrows(IllegalArgumentException.class, () -> {
-      new GpsSystem(null, str);
-    });
+    Random rnd = new Random();                          //make a moderately long array of random strings
+    int rndLength = rnd.nextInt(100);
+    String[] str = new String[rndLength];
+    for (int i = 0; i < rndLength; i++) {
+      str[i] = generateRandomNames();
+    }
+		assertThrows(IllegalArgumentException.class, () -> {
+			new GpsSystem(null, str);
+		});
 	}
+	
+	@Test
+	@DisplayName("Init Satelllite Names Not Null")
+	void initSatelliteNamesNotNull(){
+		DisplayGpsInterfaceStub stub = new DisplayGpsInterfaceStub();
+		assertThrows(IllegalArgumentException.class, () -> {
+			new GpsSystem(stub, null);
+		});
+	}
+	
+	@Test
+	@DisplayName("Init Satelllite Names Not Empty")
+	void initSatelliteNamesNotEmpty(){
+		DisplayGpsInterfaceStub stub = new DisplayGpsInterfaceStub();
+    Random rnd = new Random();                                    //make a moderately long array of random strings
+    int rndLength = rnd.nextInt(100);
+    String[] str = new String[rndLength];
+    for (int i = 0; i < rndLength; i++) {
+      str[i] = generateRandomNames();
+    }
+    str[rnd.nextInt(rndLength)] = "";                              //set random item in array to be empty
+		assertThrows(IllegalArgumentException.class, () -> {
+			new GpsSystem(stub, str);
+		});
+	}
+  
+	@Test
+	@DisplayName("GPS Systems Constructed Successfully")
+  void gpsSystemsConstructedSuccessfully(){
+    try {
+      DisplayGpsInterfaceStub stub = new DisplayGpsInterfaceStub();
+      Random rnd = new Random();                          //make a moderately long array of random strings
+      int rndLength = rnd.nextInt(100);
+      String[] str = new String[rndLength];
+      for (int i = 0; i < rndLength; i++) {
+        str[i] = generateRandomNames();
+      }
+      GpsSystem gpsSystem = new GpsSystem(stub, str);
+    } catch (Exception e) {
+      fail(e.getMessage());
+    }
+  }
+  
+  @Test
+  @DisplayName("Use Case 1 Run Successfully")
+  void useCase1RunSuccessfully(){
+    try {
+      Scanner scan = new Scanner(System.in);
+      DisplayGpsInterfaceStub stub = new DisplayGpsInterfaceStub();
+      Random rnd = new Random();    
+      int rndLength = rnd.nextInt(100);
+      String[] str = new String[rndLength];
+      for (int i = 0; i < rndLength; i++) {
+        str[i] = generateRandomNames();
+      }
+      GpsSystem gpsSystem = new GpsSystem(stub, str);
+      gpsSystem.runUseCase1(scan);
+    } catch (Exception e) {
+      fail(e.getMessage());
+    }
+  }
+  
+  
+  
+  private String generateRandomNames() { //Make a random string for Satellite Name, borrowed from Tessa, thanks!
+    int length = 10;
+    Random random = new Random();
+    String alphabet = "abcdefghijklmnopqrstuvwxyz";
+    char [] rndString = new char[length];
+
+    for (int i = 0; i < length; i++) { //Create array of characters for desired length
+      rndString[i] = alphabet.charAt(random.nextInt(alphabet.length())); 
+    }
+    
+    String createdString = new String(rndString);
+    return createdString;
+  }
+  
+  private LocationDataPoint generateRandomLocationDataPoint() {  //borrowed from Tessa again, thanks!
+    return new LocationDataPoint(
+      (float) (Math.random() * 180 - 90),
+      (float) (Math.random() * 360 - 180),
+      LocalDateTime.of(
+        (int) (Math.random() * 50 + 1970),
+        (int) (Math.random() * 12 + 1),
+        (int) (Math.random() * 28 + 1),
+        (int) (Math.random() * 24),
+        (int) (Math.random() * 60)
+      )
+    );
+  }
+  
+  private float makeRandomPos() {   //will return a float value between -90.0 and 90.0
+    Random rnd = new Random();
+    return rnd.nextFloat()*180-90;
+  }
 }
