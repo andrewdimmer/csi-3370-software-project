e798e0c5387ee828258c14e077780180ad4408e3
Ahartman3 <43781478+Ahartman3@users.noreply.github.com>
2020-11-30 12:45:59 -0500

    Update DisplayCommManagerImplementation.java

--- src/main/java/edu/oakland/production/display02/DisplayCommManagerImplementation.java
+++ src/main/java/edu/oakland/production/display02/DisplayCommManagerImplementation.java
@@ -1,17 +1,24 @@
 package edu.oakland.production.display02;
 
-import edu.oakland.helper.admin.*;
+import edu.oakland.helper.admin.LocationDataPoint;
+import edu.oakland.helper.admin.TrackData;
 import edu.oakland.production.display02.DisplayCommManager;
 import edu.oakland.production.middleware02.MiddlewareCommInterface;
 import java.lang.IllegalArgumentException;
 
 public class DisplayCommManagerImplementation implements DisplayCommManager {
 
-    MiddlewareCommInterface middleware;
-
+    private MiddlewareCommInterface middleware;
     public int rfid;
+    
+    public DisplayCommManagerImplementation(MiddlewareCommInterface middlewareCommInterface) {
+        if (middlewareCommInterface == null) {
+            throw new IllegalArgumentException("processor cannot be null");
+        }
+        this.middleware = middlewareCommInterface;
+    }
 
-    public TrackData receiveRfid(int rfidNum){
+    public TrackData receiveRfid(int rfidNum) {
         rfid = rfidNum;
         return evaluateRfidRequest(rfid);
     }
@@ -20,7 +27,7 @@ public class DisplayCommManagerImplementation implements DisplayCommManager {
         return passRfidToMiddlewareCommInterface(rfid);
     }
 
-    public TrackData passRfidToMiddlewareCommInterface(int rfid){
+    public TrackData passRfidToMiddlewareCommInterface(int rfid) {
         if (middleware == null) {
             throw new IllegalArgumentException("MiddlewareCommInterface cannot be null");
         }
