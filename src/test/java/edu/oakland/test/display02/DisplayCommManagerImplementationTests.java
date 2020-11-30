3319d0d1aecfa55029471f1ce6c61b3d3d8e05a0
Ahartman3 <43781478+Ahartman3@users.noreply.github.com>
2020-11-29 20:21:19 -0500

    Add files via upload

---
+++ src/test/java/edu/oakland/test/display02/DisplayCommManagerImplementationTests.java
@@ -0,0 +1,38 @@
+package edu.oakland.test.display02;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import edu.oakland.helper.admin.*;
+import edu.oakland.production.display02.DisplayCommManager;
+import edu.oakland.production.display02.DisplayCommManagerImplementation;
+import edu.oakland.production.middleware02.MiddlewareCommInterface;
+import edu.oakland.test.display02.MiddlewareCommInterfaceStub;
+import java.lang.IllegalArgumentException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Display Comm Manager Implementation Tests")
+public class DisplayCommManagerImplementationTests {
+
+  @Test
+  @DisplayName("RFID In Is the same as Rfid Out")
+  void rfidInIsRfidOut() {
+    int rfid = generateRandomRfid();
+    DisplayCommManagerImplementation displayComm = new DisplayCommManagerImplementation();
+    displayComm.receiveRfid(rfid);
+    assertEquals(rfid, displayComm.rfid);
+  }
+  
+  private int generateRandomRfid() {
+    return (int) Math.random();
+  }
+
+  @Test 
+  @DisplayName("MiddlewareCommInterface is not null")
+  void middlewareIsNotNull() {
+    assertThrows(IllegalArgumentException.class, () -> {
+      new DisplayCommManagerImplementation(null);
+    });
+  }
+}
