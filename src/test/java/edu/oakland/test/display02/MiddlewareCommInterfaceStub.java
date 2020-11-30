7019bb9530264bc5a2b5840b458f57f3eafe1ac4
Ahartman3 <43781478+Ahartman3@users.noreply.github.com>
2020-11-29 20:21:01 -0500

    Update MiddlewareCommInterfaceStub.java

--- src/test/java/edu/oakland/test/display02/MiddlewareCommInterfaceStub.java
+++ src/test/java/edu/oakland/test/display02/MiddlewareCommInterfaceStub.java
@@ -1,1 +1,12 @@
+package edu.oakland.test.display02;
 
+import edu.oakland.helper.admin.*;
+import edu.oakland.production.display02.DisplayCommManager;
+
+
+public class MiddlewareCommInterfaceStub{
+    public TrackData requestRfid(int rfid) {
+        
+    }
+
+}
