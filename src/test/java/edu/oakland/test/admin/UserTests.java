6effaa17f4ebbbd07c8a8f1bfab9fd14dd07a4e9
TessaPeruzzi <54461147+TessaPeruzzi@users.noreply.github.com>
2020-11-30 09:04:13 -0500

    [Admin] Build User
    **A couple tests still need to be complete

---
+++ src/test/java/edu/oakland/test/admin/UserTests.java
@@ -0,0 +1,42 @@
+package edu.oakland.test.UserTests;
+
+import edu.oakland.production.admin.User;
+import edu.oakland.production.display02.DisplayCommInterface;
+import java.lang.IllegalArgumentException;
+import java.util.Random;
+import java.util.Scanner;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("User Unit Tests")
+public class UserTests {
+
+  @Test
+  @DisplayName("Display Comm Interface Is Not Null")
+  void displayCommInterfaceIsNotNull() {
+    assertThrows(IllegalArgumentException.class, () -> {
+      new User(null);
+    });
+  }
+
+  @Test
+  @DisplayName("Scanner is Not Null") 
+  void userInputIsScanInput() {
+    /*User user=new User(new DisplayCommInterface()); 
+    assertThrows(IllegalArgumentException.class, () -> {
+      user.runUseCase1(null);
+    });*/
+  } //I don't think I am thinking about this correctly
+  //Cannot do it this way because an object cannot be created from interface
+
+  @Test
+  @DisplayName("TrackDataIsRequested")
+  void trackDataIsRequested() {
+  
+  }  
+  
+
+}
+
