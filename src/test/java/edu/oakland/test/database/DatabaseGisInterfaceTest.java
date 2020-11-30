304d396e3c8fb6518fcf413b52e210f821a13c1d
Robertson Brinker <60048070+robertsonbrinker@users.noreply.github.com>
2020-11-29 22:17:58 -0500

    Update DatabaseGisInterfaceTest.java

--- src/test/java/edu/oakland/test/database/DatabaseGisInterfaceTest.java
+++ src/test/java/edu/oakland/test/database/DatabaseGisInterfaceTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import edu.oakland.helper.admin.LocationDataPoint;
 import edu.oakland.production.database.DatabaseGisInterfaceImplementation;
-import edu.oakland.production.database.DatabaseGisManagerStub;
+import edu.oakland.test.database.DatabaseGisManagerStub;
 import java.lang.IllegalArgumentException;
 import java.time.LocalDateTime;
 import java.util.Random;
