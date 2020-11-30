533cc9ec17be00fa3ffcf337b19f33a66709bf00
Dbaker19972 <48261953+Dbaker19972@users.noreply.github.com>
2020-11-30 18:20:32 -0500

    Update DisplayCommInterfaceImplementation.java

--- src/main/java/edu/oakland/production/display02/DisplayCommInterfaceImplementation.java
+++ src/main/java/edu/oakland/production/display02/DisplayCommInterfaceImplementation.java
@@ -1,17 +1,42 @@
+
 package edu.oakland.production.display02;
 
-
+import edu.oakland.helper.admin.LocationDataPoint;
 import edu.oakland.helper.admin.TrackData;
+import edu.oakland.production.display02.DisplayCommInterface;
+import edu.oakland.production.display02.DisplayComm2WayInterface;
+import java.lang.IllegalArgumentException;
 
+/**
+* This class receives the rfid, evaluates it,
+* then passes it along to Comm2Way.
+*
+*/
+public class DisplayCommInterfaceImplementation implements DisplayCommInterface{
 
-public class DisplayCommInterfaceImplementation implements DisplayCommInterface {
-  public int rfid;
-  DisplayComm2WayInterfaceImplementation instance = new DisplayComm2WayInterfaceImplementation();
-
-  /** This recieves the rfid number from the user then passes it off to comm 2 way. */
-  public TrackData receiveRfidRequest(int rfidNum) {
-    rfid = rfidNum;
-    TrackData trackData = instance.passRfidRequest(rfidNum);
-    return trackData;
+  private DisplayComm2WayInterface comm2Way;
+    
+  /**
+  * Creates a DisplayCommInterfaceImplementation to receive the rfid.
+  * param DisplatComm2WayInterface: The class that DisplayCommInterface will pass the rfid to.
+  *
+  */
+  public DisplayCommInterfaceImplementation(DisplayComm2WayInterface displayComm2WayInterface) {
+    if (displayComm2WayInterface == null) {
+      throw new IllegalArgumentException("processor cannot be null");
+    }
+    this.comm2Way = displayComm2WayInterface;
   }
+
+  /**
+  * Receives the rfid from User
+  * before passing along to Comm2way.
+  *
+  * @return the TrackData
+  */
+  public TrackData evaluateRfidRequest(int rfid) {
+    return comm2Way.requestRfid(rfid);
+  }
+}
+
 }
