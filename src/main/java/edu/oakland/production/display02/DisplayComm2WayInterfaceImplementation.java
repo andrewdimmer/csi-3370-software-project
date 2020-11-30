04b9e2713a8aaf735f941b96f8b72f3052b5c1d5
Jessica Cortes <55962629+jcortes18@users.noreply.github.com>
2020-11-30 16:36:02 -0500

    Update DisplayComm2WayInterfaceImplementation.java

--- src/main/java/edu/oakland/production/display02/DisplayComm2WayInterfaceImplementation.java
+++ src/main/java/edu/oakland/production/display02/DisplayComm2WayInterfaceImplementation.java
@@ -13,7 +13,7 @@ public class DisplayComm2WayInterfaceImplementation implements DisplayComm2WayIn
   /**
   * Creates a DisplayComm2WayInterfaceImplementation to receive the rfid.
   *
-  * @param DisplayCommManagerImplementation: The class that DisplayComm2Way will pass RFID to. 
+  * @param DisplayCommManagerImplementation The class that DisplayComm2Way will pass RFID to. 
   *
   */
   public DisplayComm2WayInterfaceImplementation(DisplayCommManagerImplementation displayCommManager) {
