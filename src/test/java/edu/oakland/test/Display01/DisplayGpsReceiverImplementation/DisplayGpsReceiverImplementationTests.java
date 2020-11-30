07c74268d3e06adefd14016d40d928789c708b05
EC01EC <73436762+EC01EC@users.noreply.github.com>
2020-11-29 20:43:27 -0500

    Update DisplayGpsReceiverImplementationTests.java
    Style guide "fixes"?

--- src/test/java/edu/oakland/test/Display01/DisplayGpsReceiverImplementation/DisplayGpsReceiverImplementationTests.java
+++ src/test/java/edu/oakland/test/Display01/DisplayGpsReceiverImplementation/DisplayGpsReceiverImplementationTests.java
@@ -1,13 +1,13 @@
 package edu.oakland.test.Display01.DisplayGpsReceiverImplementation;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import java.lang.IllegalArgumentException;
 import edu.oakland.helper.admin.Satellite;
 import edu.oakland.production.display01.DisplayGpsManagerImplementation;
 import edu.oakland.production.display01.DisplayGpsReceiverImplementation;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import java.lang.IllegalArgumentException;
+
 
 @DisplayName ("Display GPS Receiver Implementation")
 public class DisplayGpsReceiverImplementationTests {
