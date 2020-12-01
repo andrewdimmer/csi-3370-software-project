c19ff63afde9a7b73fb093e8b8ced8715b582271
McKenzie King <38590958+mckenzieking@users.noreply.github.com>
2020-11-30 20:45:10 -0500

    [Middleware_02] Build MiddlewareCommDatabaseInterface (#111)
    * add DatabaseCommInterface stub and implementation tests
    
    * finished database stub and interface classes
    
    * Update DatabaseCommInterfaceStub.java
    
    * Update MiddlewareCommDatabaseInterfaceImplementation.java
    
    * Update MiddlewareCommDatabaseInterfaceImplementation.java
    
    * Update MiddlewareCommDatabaseInterfaceImplementationTests.java
    
    * Update MiddlewareCommDatabaseInterfaceImplementation.java
    
    * Update MiddlewareCommDatabaseInterfaceImplementation.java
    
    * Update MiddlewareCommDatabaseInterfaceImplementation.java
    
    * Update MiddlewareCommDatabaseInterfaceImplementation.java
    
    * Update MiddlewareCommDatabaseInterfaceImplementation.java
    
    * Update MiddlewareCommDatabaseInterfaceImplementationTests.java
    
    * remove some newlines
    
    * Update MiddlewareCommDatabaseInterfaceImplementationTests.java
    
    * separate method def
    
    * more changes for the style guide
    
    * Update MiddlewareCommDatabaseInterfaceImplementationTests.java
    
    * fix style guide problems with MiddlewareCommDatabaseInterfaceImplementation
    
    * Whitespace style guide fix
    
    * another whitespace fix...
    
    * fix indentation of DatabaseCommInterfaceStub
    
    * spacing fixes for DatabaseCommInterfaceStub
    
    * small whitespace fix
    
    * Javadoc comment for CommDatabaseInterfaceImplementation
    
    * [Middleware_02] Minor CommDatabaseInterface Cleanup Before Merge
    
    Co-authored-by: Rio Capollari <rcapollari@oakland.edu>
    Co-authored-by: kwillard34 <60444686+kwillard34@users.noreply.github.com>
    Co-authored-by: tomtomtomcat <55642071+tomtomtomcat@users.noreply.github.com>
    Co-authored-by: Andrew Dimmer <contactandrewd@gmail.com>

---
+++ src/main/java/edu/oakland/production/middleware02/MiddlewareCommDatabaseInterfaceImplementation.java
@@ -0,0 +1,29 @@
+package edu.oakland.production.middleware02;
+
+import edu.oakland.helper.admin.TrackData;
+import edu.oakland.production.database.DatabaseCommInterface;
+import edu.oakland.production.middleware02.MiddlewareCommDatabaseInterface;
+
+
+public class MiddlewareCommDatabaseInterfaceImplementation implements 
+      MiddlewareCommDatabaseInterface {
+
+  private DatabaseCommInterface databaseCommInterface;
+  
+  /**
+   * The constructor for MiddlewareCommDatabaseInterfaceImplementation.
+   */
+  public MiddlewareCommDatabaseInterfaceImplementation(
+      DatabaseCommInterface databaseCommInterface
+  ) {
+    if (databaseCommInterface == null) {
+      throw new IllegalArgumentException("databaseCommInterface cannot be null");
+    }
+        
+    this.databaseCommInterface = databaseCommInterface;
+  }
+  
+  public int checkCurrentRfid() {
+    return databaseCommInterface.receiveGetRfidRequest();
+  } 
+}
