d4493692b669c89bbb80bc0f152b4225c62bb04e
Dbaker19972 <48261953+Dbaker19972@users.noreply.github.com>
2020-11-30 19:31:20 -0500

    [Display_02] DisplayCommInterface Test and Implementation (#134)
    * Display CommInterface Test and IMP
    
    * Add Display Comm Test
    
    * update style
    
    * updated display comm interface implementation and test (#146)
    
    Manual roll back then re-commit without the /bin/ folder
    
    * Update
    
    * Delete DisplayCommIterfaceImplementationTest.java
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * Update DisplayCommInterfaceImplementation.java
    
    * Delete DisplayComm2WayInterfaceImplementation.java
    
    * update
    
    * Delete DisplayCommInterfaceImplementationTest.java
    
    * update
    
    * update
    
    * Update DisplayCommInterfaceImplementationTests.java
    
    * Update DisplayCommInterfaceImplementationTests.java
    
    * Update DisplayCommInterfaceImplementation.java
    
    * Update src/main/java/edu/oakland/production/display02/DisplayCommInterfaceImplementation.java
    
    Co-authored-by: Andrew Dimmer <43498863+andrewdimmer@users.noreply.github.com>
    
    Co-authored-by: Andrew Dimmer <contactandrewd@gmail.com>
    Co-authored-by: Andrew Dimmer <43498863+andrewdimmer@users.noreply.github.com>

---
+++ src/main/java/edu/oakland/production/display02/DisplayCommInterfaceImplementation.java
@@ -0,0 +1,39 @@
+package edu.oakland.production.display02;
+
+import edu.oakland.helper.admin.LocationDataPoint;
+import edu.oakland.helper.admin.TrackData;
+import edu.oakland.production.display02.DisplayComm2WayInterface;
+import edu.oakland.production.display02.DisplayCommInterface;
+import java.lang.IllegalArgumentException;
+
+/**
+* This class receives the rfid, evaluates it,
+* then passes it along to Comm2Way.
+*
+*/
+public class DisplayCommInterfaceImplementation implements DisplayCommInterface {
+
+  private DisplayComm2WayInterface comm2Way;
+
+  /**
+  * Creates a DisplayCommInterfaceImplementation to receive the rfid.
+  * param DisplatComm2WayInterface: The class that DisplayCommInterface will pass the rfid to.
+  *
+  */
+  public DisplayCommInterfaceImplementation(DisplayComm2WayInterface displayComm2WayInterface) {
+    if (displayComm2WayInterface == null) {
+      throw new IllegalArgumentException("displayComm2WayInterface cannot be null");
+    }
+    this.comm2Way = displayComm2WayInterface;
+  }
+
+  /**
+  * Receives the rfid from User
+  * before passing along to Comm2way.
+  *
+  * @return the TrackData
+  */
+  public TrackData receiveRfidRequest(int rfidNum) {
+    return comm2Way.passRfidRequest(rfidNum);
+  }
+}
