10317631f9e363a5145b01daf76db8466a6f77b8
jmccloud827 <60444521+jmccloud827@users.noreply.github.com>
2020-11-30 20:09:13 -0500

    [Display_02] Update Tests

--- src/test/java/edu/oakland/test/display02/DisplayComm2WayInterfaceImplementationTests.java
+++ src/test/java/edu/oakland/test/display02/DisplayComm2WayInterfaceImplementationTests.java
@@ -1,12 +1,6 @@
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import edu.oakland.helper.admin.LocationDataPoint;
-import edu.oakland.helper.admin.TrackData;
-import edu.oakland.production.display02.DisplayCommManager;
-import edu.oakland.production.display02.DisplayCommManagerImplementation;
-import edu.oakland.production.display02.DisplayComm2WayInterface;
 import edu.oakland.production.display02.DisplayComm2WayInterfaceImplementation;
-import java.lang.IllegalArgumentException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
