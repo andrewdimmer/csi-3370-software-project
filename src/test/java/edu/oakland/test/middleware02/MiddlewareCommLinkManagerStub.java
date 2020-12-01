2b74eb29c23c447cfa3f4ae2704d9da7fa774bf2
NMoua-Edu <73411759+NMoua-Edu@users.noreply.github.com>
2020-11-30 19:44:11 -0500

    Style changes

--- src/test/java/edu/oakland/test/middleware02/MiddlewareCommLinkManagerStub.java
+++ src/test/java/edu/oakland/test/middleware02/MiddlewareCommLinkManagerStub.java
@@ -3,11 +3,19 @@ package edu.oakland.test.middleware02;
 import edu.oakland.helper.admin.LocationDataPoint;
 import edu.oakland.helper.admin.TrackData;
 import edu.oakland.production.middleware02.MiddlewareCommLinkManager;
+
 /**
  * This class shall get the current rfid and set it in trackdata.
  */
 
 public class MiddlewareCommLinkManagerStub implements MiddlewareCommLinkManager {
+  
+/**
+ *
+ * This class set the status message in track data to a string of Rfid
+ *
+ */
+
   public TrackData checkCurrentRfid(int rfid) {
     TrackData trackD = new TrackData(new LocationDataPoint[0]);
     trackD.setStatusMessage(String.valueOf(rfid));
