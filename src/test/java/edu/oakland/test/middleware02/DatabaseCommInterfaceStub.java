84bdea659eab5100f38b3162cb91585ba6f4fe9b
tomtomtomcat <55642071+tomtomtomcat@users.noreply.github.com>
2020-11-30 20:02:59 -0500

    spacing fixes for DatabaseCommInterfaceStub

--- src/test/java/edu/oakland/test/middleware02/DatabaseCommInterfaceStub.java
+++ src/test/java/edu/oakland/test/middleware02/DatabaseCommInterfaceStub.java
@@ -6,21 +6,27 @@ import edu.oakland.production.database.DatabaseCommInterface;
 
 public class DatabaseCommInterfaceStub implements DatabaseCommInterface {
   private int rfid;
+  
   public DatabaseCommInterfaceStub(int rfid){
     this.rfid = rfid;
   }
+  
   public int receiveGetRfidRequest() {
     return rfid;
   }
+  
   public String receiveGetModeRequest() {
     return null;
   }
+  
   public void storeTrackDataRequest(TrackData trackData) {
       
   }
+  
   public LocationDataPoint receiveGetLocationDataPointRequest(int offset) {
     return null;
   }
+  
   public TrackData receiveGetTrackDataRequest(int offset) {
     return null;
   }
