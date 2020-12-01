607d4857c7613751c0900231d43c1d7c7d596b31
jmccloud827 <60444521+jmccloud827@users.noreply.github.com>
2020-11-30 20:06:05 -0500

    Update DisplayComm2WayInterfaceImplementation.java

--- src/main/java/edu/oakland/production/display02/DisplayComm2WayInterfaceImplementation.java
+++ src/main/java/edu/oakland/production/display02/DisplayComm2WayInterfaceImplementation.java
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 package edu.oakland.production.display02;
 
 import edu.oakland.helper.admin.LocationDataPoint;
@@ -58,64 +57,3 @@ public class DisplayComm2WayInterfaceImplementation implements DisplayComm2WayIn
     return commManager.evaluateRfidRequest(rfidNum);
   }
 }
-=======
-package edu.oakland.production.display02;
-
-import edu.oakland.helper.admin.LocationDataPoint;
-import edu.oakland.helper.admin.TrackData;
-import edu.oakland.production.display02.DisplayComm2WayInterface;
-import edu.oakland.production.display02.DisplayCommManager;
-import java.lang.IllegalArgumentException;
-
-
-public class DisplayComm2WayInterfaceImplementation implements DisplayComm2WayInterface {
-  
-  private DisplayCommManager commManager;
-  
-  /**
-  * Creates a DisplayComm2WayInterfaceImplementation to receive the rfid.
-  *
-  * @param displayCommManager The class that DisplayComm2Way will pass RFID to. 
-  *
-  */
-  public DisplayComm2WayInterfaceImplementation(DisplayCommManager displayCommManager) {
-    if (displayCommManager == null) {
-      throw new IllegalArgumentException("cannot be null");
-    }
-    this.commManager = displayCommManager;
-  }
-
-  /**
-  * Receives the rfid from DisplayCommInterface
-  * before passing along.
-  *
-  * @return the TrackData
-  */
-  private TrackData receiveRfidRequest(int rfidNum) {
-
-    return determineUserRequestFormat(rfidNum);
-  }
-  
-  
-  /**
-  * Determine the rfid request format.
-  *
-  * @return the TrackData
-  */
-  private TrackData determineUserRequestFormat(int rfidNum) {
-
-    return passRfidRequest(rfidNum);
-  }
-  
-  
-  /**
-  * Passes the evaluated rfid along to DisplayCommManagerInterface.
-  *
-  * @return the TrackData
-  */
-  public TrackData passRfidRequest(int rfidNum) {
-
-    return commManager.evaluateRfidRequest(rfidNum);
-  }
-}
->>>>>>> Stashed changes
