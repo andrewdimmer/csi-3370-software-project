d133a0c5356bee1fcdcb4e48ce2895db5f698789
MathewYaldo <55512598+MathewYaldo@users.noreply.github.com>
2020-11-30 22:19:14 -0500

    [Database] Build Database CommInterface (#138)
    * Update
    
    * Updated stub and class
    
    * update
    
    * update
    
    * update styleguide
    
    * Update DatabaseCommInterfaceTest.java
    
    * Update DatabaseCommInterfaceTest.java
    
    * updated pt 43
    
    * updatedededed
    
    * Update src/main/java/edu/oakland/production/database/DatabaseCommInterfaceClass.java
    
    Co-authored-by: Andrew Dimmer <43498863+andrewdimmer@users.noreply.github.com>
    
    * Update src/test/java/edu/oakland/test/database/DatabaseCommInterfaceTest.java
    
    Co-authored-by: Andrew Dimmer <43498863+andrewdimmer@users.noreply.github.com>
    
    * Update src/test/java/edu/oakland/test/database/DatabaseCommManagerStub.java
    
    Co-authored-by: Andrew Dimmer <43498863+andrewdimmer@users.noreply.github.com>
    
    * Update src/test/java/edu/oakland/test/database/DatabaseCommManagerStub.java
    
    Co-authored-by: Andrew Dimmer <43498863+andrewdimmer@users.noreply.github.com>
    
    * Update src/test/java/edu/oakland/test/database/DatabaseCommInterfaceTest.java
    
    Co-authored-by: Andrew Dimmer <43498863+andrewdimmer@users.noreply.github.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Andrew Dimmer <43498863+andrewdimmer@users.noreply.github.com>
    
    * Update DatabaseCommInterfaceTest.java
    
    * Update and rename DatabaseCommInterfaceClass.java to DatabaseCommInterfaceImplementation.java
    
    * Update DatabaseCommInterfaceTest.java
    
    * Fix errors so that this runs
    
    * Delete .project
    
    * Update DatabaseCommInterfaceTest.java
    
    * Update DatabaseCommManagerStub.java
    
    * Update DatabaseCommInterfaceTest.java
    
    * Update DatabaseCommInterfaceTest.java
    
    * Update DatabaseCommManagerStub.java
    
    * Update DatabaseCommInterfaceTest.java
    
    * Update DatabaseCommInterfaceTest.java
    
    * Update DatabaseCommInterfaceTest.java
    
    * Update DatabaseCommManagerStub.java
    
    * Update DatabaseCommManagerStub.java
    
    * Update DatabaseCommInterfaceTest.java
    
    * Update DatabaseCommInterfaceTest.java
    
    * Update DatabaseCommInterfaceTest.java
    
    * Update src/test/java/edu/oakland/test/database/DatabaseCommManagerStub.java
    
    Co-authored-by: Andrew Dimmer <43498863+andrewdimmer@users.noreply.github.com>
    
    * Update src/test/java/edu/oakland/test/database/DatabaseCommInterfaceTest.java
    
    Co-authored-by: Andrew Dimmer <43498863+andrewdimmer@users.noreply.github.com>
    
    Co-authored-by: kylepoterek <60444568+kylepoterek@users.noreply.github.com>
    Co-authored-by: Endrit <endritzenuni112@gmail.com>
    Co-authored-by: Jake Gleason <jrgleason@oakland.edu>
    Co-authored-by: Andrew Dimmer <43498863+andrewdimmer@users.noreply.github.com>
    Co-authored-by: Robertson Brinker <60048070+robertsonbrinker@users.noreply.github.com>

---
+++ src/main/java/edu/oakland/production/database/DatabaseCommInterfaceImplementation.java
@@ -0,0 +1,46 @@
+package edu.oakland.production.database;
+
+import edu.oakland.helper.admin.LocationDataPoint;
+import edu.oakland.helper.admin.TrackData;
+import edu.oakland.production.database.DatabaseCommManager;
+
+public class DatabaseCommInterfaceImplementation implements DatabaseCommInterface {
+  
+  DatabaseCommManager databaseCommManager;
+
+  /**
+   * Creates an database interface to middleware
+   * to provide methods for managing Comm info.
+   *
+   * @param databaseCommManager is an instance of the DatabaseCommManagerImplementation class.
+   */
+
+  public DatabaseCommInterfaceImplementation(DatabaseCommManager databaseCommManager) {
+    if (databaseCommManager == null) {
+      throw new IllegalArgumentException("manager cannot be null");
+    }
+    
+    this.databaseCommManager = databaseCommManager;
+  }
+  
+  public int receiveGetRfidRequest() {
+    return databaseCommManager.passGetRfidRequest();
+  }
+  
+  public LocationDataPoint receiveGetLocationDataPointRequest(int offset) {
+    return databaseCommManager.passGetLocationDataPointRequest(offset);
+  }
+  
+  public TrackData receiveGetTrackDataRequest(int offset) {
+    return databaseCommManager.passGetTrackDataRequest(offset);
+  }
+  
+  public void storeTrackDataRequest(TrackData trackData) {
+    databaseCommManager.passStoreTrackDataRequest(trackData);
+  }
+  
+  public String receiveGetModeRequest() {
+    return databaseCommManager.passGetModeRequest(); 
+  }
+  
+}
