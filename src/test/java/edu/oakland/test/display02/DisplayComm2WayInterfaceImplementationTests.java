fc6aa577babe165c13873dbe2b45f2d94a0cf24f
jmccloud827 <60444521+jmccloud827@users.noreply.github.com>
2020-11-30 20:02:10 -0500

    [Display_02] Update Tests

--- src/test/java/edu/oakland/test/display02/DisplayComm2WayInterfaceImplementationTests.java
+++ src/test/java/edu/oakland/test/display02/DisplayComm2WayInterfaceImplementationTests.java
@@ -1,6 +1,7 @@
 package edu.oakland.test.display02;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+<<<<<<< Updated upstream
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import edu.oakland.production.display02.DisplayComm2WayInterface;
@@ -25,4 +26,29 @@ public class DisplayComm2WayInterfaceImplementationTests {
     assertNotNull(manager, "Checking if class is null");
   }
 
+=======
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import edu.oakland.helper.admin.LocationDataPoint;
+import edu.oakland.helper.admin.TrackData;
+import edu.oakland.production.display02.DisplayCommManager;
+import edu.oakland.production.display02.DisplayCommManagerImplementation;
+import edu.oakland.production.display02.DisplayComm2WayInterface;
+import edu.oakland.production.display02.DisplayComm2WayInterfaceImplementation;
+import java.lang.IllegalArgumentException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("DisplayComm2WayInterfaceImplementation Unit Tests")
+
+public class DisplayComm2WayInterfaceImplementationTests {
+
+  @Test 
+  @DisplayName("CommManager is not null")
+  void commManagerIsNotNull() {
+    assertThrows(IllegalArgumentException.class, () -> {
+      new DisplayComm2WayInterfaceImplementation(null);
+    });
+  }
+>>>>>>> Stashed changes
 }
