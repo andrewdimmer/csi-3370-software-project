27d44ef7651a5bd6b0078b394845d90d544d586c
atpoynter <55966011+atpoynter@users.noreply.github.com>
2020-12-01 01:22:09 -0500

    Update DisplayGpsManagerImplementation.java

--- src/main/java/edu/oakland/production/display01/DisplayGpsManagerImplementation.java
+++ src/main/java/edu/oakland/production/display01/DisplayGpsManagerImplementation.java
@@ -4,15 +4,17 @@ import edu.oakland.helper.admin.Satellite;
 import edu.oakland.helper.display01.SatelliteSignalCheckRequest;
 
 public class DisplayGpsManagerImplementation implements DisplayGpsManager {
-    
-  public String receiveGpsSignalStrength(Satellite satelliteSignal) {
-    if (satelliteSignal.getStrength() > 4) {
-      return "Acceptable";
-    }
-    else {
-      return "Poor";
+  
+  public DisplayGpsManager manager;
+
+  public DisplayGpsManagerImplementation(DisplayGpsManager manager) {
+    this.manager = manager;
   }
-}
+
+  psSignalStrength(Satellite satelliteSignal) {
+  
+  }
+
   public SatelliteSignalCheckRequest passGpsSignalStrength(Satellite satelliteSignal) {
     return null;
 
