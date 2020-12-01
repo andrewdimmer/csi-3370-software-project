a90a289d7cb2ef0b4ab0dac93fbd25895d4c80f0
atpoynter <55966011+atpoynter@users.noreply.github.com>
2020-11-30 22:34:30 -0500

    Created MiddlewareGisInterfaceStub.java

---
+++ src/test/java/edu/oakland/test/display01/MIddlewareGisInterfaceStub.java
@@ -0,0 +1,13 @@
+package edu.oakland.test.display01;
+import edu.oakland.production.middleware01.MiddlewareGisInterface;
+
+public class MiddlewareGisInterface implements MiddlewareGisInterfaceStub {
+
+    public String receiveGpsSignalStrengths(Satellite satelliteSignal){
+
+    }
+
+    public String receiveGpsSignalStrengths(String satelliteName){
+  }
+}
+
