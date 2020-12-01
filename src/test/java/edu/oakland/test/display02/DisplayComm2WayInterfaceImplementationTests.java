fd4a62b7304144842c9e65e26340cc37006798e2
jmccloud827 <60444521+jmccloud827@users.noreply.github.com>
2020-11-30 20:05:16 -0500

    Update DisplayComm2WayInterfaceImplementationTests.java

--- src/test/java/edu/oakland/test/display02/DisplayComm2WayInterfaceImplementationTests.java
+++ src/test/java/edu/oakland/test/display02/DisplayComm2WayInterfaceImplementationTests.java
@@ -1,32 +1,3 @@
-package edu.oakland.test.display02;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-<<<<<<< Updated upstream
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
-import edu.oakland.production.display02.DisplayComm2WayInterface;
-import edu.oakland.production.display02.DisplayCommManager;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-
-@DisplayName("Display Comm 2 Way Interface Implementation Tests")
-public class DisplayComm2WayInterfaceImplementationTests {
-
-  @Test
-  void rfidInIsRfidOut() {
-    int rfid = 1101;  //rfid number?
-    DisplayComm2WayInterfaceImplementation comm2Way = new DisplayComm2WayInterfaceImplementation();
-    int out = comm2Way.rfid;
-    assertEquals(rfid, out);
-  }
-
-  @Test
-  void displayComm2WayIsNotNull() {
-    DisplayCommManager manager = new DisplayCommManager();
-    assertNotNull(manager, "Checking if class is null");
-  }
-
-=======
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import edu.oakland.helper.admin.LocationDataPoint;
@@ -50,5 +21,4 @@ public class DisplayComm2WayInterfaceImplementationTests {
       new DisplayComm2WayInterfaceImplementation(null);
     });
   }
->>>>>>> Stashed changes
 }
