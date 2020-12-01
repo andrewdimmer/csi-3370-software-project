1d0e60ce59ce32161a555e50b378104aacdf49e5
jmccloud827 <60444521+jmccloud827@users.noreply.github.com>
2020-11-30 22:40:32 -0500

    update Tests

---
+++ src/test/java/edu/oakland/test/display02/DisplayCommManagerStub.java
@@ -0,0 +1,17 @@
+package edu.oakland.test.display02;
+
+import edu.oakland.production.display02.DisplayCommManager;
+import edu.oakland.production.display02.DisplayComm2WayInterface;
+import edu.oakland.helper.admin.LocationDataPoint;
+import edu.oakland.helper.admin.TrackData;
+public class DisplayCommManagerStub implements DisplayCommManager {
+    /**
+  * Stub method, recieves the rfid
+  * and returns a blank TrackData.
+  */
+  public TrackData requestRfid(int rfid) {
+    TrackData trackStub = new TrackData(new LocationDataPoint[0]);
+    trackStub.setStatusMessage(String.valueOf(rfid));
+    return trackStub;
+  }
+}
