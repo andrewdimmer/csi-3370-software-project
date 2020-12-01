0d79414c822c74f9204b36b823c5f9af98f36e0d
DLidz <46999371+DLidz@users.noreply.github.com>
2020-11-30 23:32:13 -0500

    Style Guide Fixes for Tests

--- src/test/java/edu/oakland/test/display01/DisplayGpsInterfaceImplementationTests.java
+++ src/test/java/edu/oakland/test/display01/DisplayGpsInterfaceImplementationTests.java
@@ -2,42 +2,52 @@ package edu.oakland.test.display01;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import java.util.Random;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+
 import edu.oakland.helper.admin.Satellite;
 import edu.oakland.production.display01.DisplayGpsInterfaceImplementation;
 import edu.oakland.production.display01.DisplayGpsReceiver;
 import edu.oakland.test.display01.DisplayGpsReceiverStub;
+import java.util.Random;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
-  @DisplayName("Display GPS Interface Unit Tests")
-    public class DisplayGpsInterfaceImplementationTests {
-      @Test
-      @DisplayName("Verifying Signal In is Signal Out")
-      void SignalInIsSignalOut() {
-        Satellite s = new Satellite("New Satellite", 7);
-        DisplayGpsInterfaceImplementation d = new DisplayGpsInterfaceImplementation(new DisplayGpsReceiverStub());
-        assertEquals(d.receiveGpsSignal(s),"New Satellite");
-      }
-      @Test
-      @DisplayName("Verifying Satellite Name in is Satellite Name Out")
-      void SatelliteInIsSatelliteOutReportLoss() {
-        Satellite s = new Satellite("New Satellite", 7);
-        DisplayGpsInterfaceImplementation d = new DisplayGpsInterfaceImplementation(new DisplayGpsReceiverStub());
-        assertEquals(d.reportGpsSignalLoss(s), "New Satellite");
-      }
-      @Test
-      @DisplayName("Checking Satellite Signal")
-      void SatelliteInIsSatelliteOutCheck() {
-        Satellite s = new Satellite("New Satellite", 7);
-        DisplayGpsInterfaceImplementation d = new DisplayGpsInterfaceImplementation(new DisplayGpsReceiverStub());
-        assertEquals(d.checkSignalStrength(s), "New Satellite");
-      }
-      @Test
-      @DisplayName("Rechecking Satellite Signal")
-      void SatelliteInIsSatelliteOutRecheck() {
-        Satellite s = new Satellite("New Satellite", 7);
-        DisplayGpsInterfaceImplementation d = new DisplayGpsInterfaceImplementation(new DisplayGpsReceiverStub());
-        assertEquals(d.recheckSignalStrength(s), "New Satellite");
-    }
+
+@DisplayName("Display GPS Interface Unit Tests")
+public class DisplayGpsInterfaceImplementationTests {
+  @Test
+  @DisplayName("Verifying Signal In is Signal Out")
+  void signalInIsSignalOut() {
+    Satellite s = new Satellite("New Satellite", 7);
+    DisplayGpsInterfaceImplementation d;
+    d = new DisplayGpsInterfaceImplementation(new DisplayGpsReceiverStub());
+    assertEquals(d.receiveGpsSignal(s), "New Satellite");
   }
+
+  @Test
+  @DisplayName("Verifying Satellite Name in is Satellite Name Out")
+  void satelliteInIsSatelliteOutReportLoss() {
+    Satellite s = new Satellite("New Satellite", 7);
+    DisplayGpsInterfaceImplementation d;
+    d = new DisplayGpsInterfaceImplementation(new DisplayGpsReceiverStub());
+    assertEquals(d.reportGpsSignalLoss(s), "New Satellite");
+  }
+
+  @Test
+  @DisplayName("Checking Satellite Signal")
+  void satelliteInIsSatelliteOutCheck() {
+    Satellite s = new Satellite("New Satellite", 7);
+    DisplayGpsInterfaceImplementation d;
+    d = new DisplayGpsInterfaceImplementation(new DisplayGpsReceiverStub());
+    assertEquals(d.checkSignalStrength(s), "New Satellite");
+  }
+
+  @Test
+  @DisplayName("Rechecking Satellite Signal")
+  void satelliteInIsSatelliteOutRecheck() {
+    Satellite s = new Satellite("New Satellite", 7);
+    DisplayGpsInterfaceImplementation d;
+    d = new DisplayGpsInterfaceImplementation(new DisplayGpsReceiverStub());
+    assertEquals(d.recheckSignalStrength(s), "New Satellite");
+  }
+
+}
