3386cc48b702b25d34a79be5534f54dea549b652
Jessica Cortes <55962629+jcortes18@users.noreply.github.com>
2020-11-30 22:52:08 -0500

    Update TrackDataPlotter.java

--- src/main/java/edu/oakland/helper/display02/TrackDataPlotter.java
+++ src/main/java/edu/oakland/helper/display02/TrackDataPlotter.java
@@ -1,10 +1,7 @@
 package edu.oakland.helper.display02;
 
-//include jfreechart-1.0.19.jar
-//include jcommon-1.0.23.jar
-
-import edu.oakland.helper.admin.TrackData;
 import edu.oakland.helper.admin.LocationDataPoint;
+import edu.oakland.helper.admin.TrackData;
 
 import java.awt.Color;
 
@@ -26,7 +23,7 @@ import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
  * and displays both in a JFreeChart
  * Based on example from boraji.com/jfreechart-scatter-chart-example
  */
-public class TrackDataPlotter extends JFrame{
+public class TrackDataPlotter extends JFrame {
 
     private Boolean isValidTrackData;
     private float[] lat;
@@ -40,84 +37,79 @@ public class TrackDataPlotter extends JFrame{
    * data as a series and the line of best fit represented by two points
    * @param data TrackData object representing the location data to be plotted
    */
-  public TrackDataPlotter(TrackData data){
+    public TrackDataPlotter(TrackData data) { 
 
-    if(data == null){
-        throw new IllegalArgumentException("Track data is null");
-    }
-    this.isValidTrackData = data.isValid();
-    if(!isValidTrackData){
-        throw new IllegalArgumentException("Track data is invalid");
-    }
-    this.lat = extractLatFromTrackData(data);
-    this.lng = extractLngFromTrackData(data);
-    this.dataPoints = getTrackData(this.lat, this.lng);
-    if(this.dataPoints.getItemCount() == 0){
-        throw new IllegalArgumentException("Data series is empty")
-    }
-    if(this.dataPoints.getItemCount() != 5){
-        throw new IllegalArgumentException("Data series contains an invalid number of points")
-    }
-    this.fitLine = calculateLine(this.lat, this.lng);
-    if(this.fitLine.getItemCount() == 0){
-        throw new IllegalArgumentException("Fit Line representation contains no points")
-    }
-    if(this.fitLine.getItemCount() != 2){
-        throw new IllegalArgumentException("Fit line contains an invalid number of points")
-    }
-    this.chart = createChart();
-
+        if(data == null) {
+            throw new IllegalArgumentException("Track data is null");
+        }
+        this.isValidTrackData = data.isValid();
+        if(!isValidTrackData) {
+            throw new IllegalArgumentException("Track data is invalid");
+        }
+        this.lat = extractLatFromTrackData(data);
+        this.lng = extractLngFromTrackData(data);
+        this.dataPoints = getTrackData(this.lat, this.lng);
+        if(this.dataPoints.getItemCount() == 0) {
+            throw new IllegalArgumentException("Data series is empty")
+        }
+        if(this.dataPoints.getItemCount() != 5) {
+            throw new IllegalArgumentException("Data series contains an invalid number of points")
+        }
+        this.fitLine = calculateLine(this.lat, this.lng);
+        if(this.fitLine.getItemCount() == 0) {
+            throw new IllegalArgumentException("Fit Line representation contains no points")
+        }
+        if(this.fitLine.getItemCount() != 2) {
+            throw new IllegalArgumentException("Fit line contains an invalid number of points")
+        }
+        this.chart = createChart();
   }
 
   /**
    * Creates a JFreeChart from the dataPoint series and fitLine series
    * @return JFreeChart poltting the location data points and line of best fit
    */
-  private JFreeChart createChart(){
+    private JFreeChart createChart() {
 
-    // Create new series collection
-    XYSeriesCollection dataset = new XYSeriesCollection();
-    // Add series to series collection
-    dataset.addSeries(this.dataPoints);
-    dataset.addSeries(this.fitLine);
-    // Create dataset with series collection
-    XYDataset dataPoints = dataset;
-    // Create Scatter Plot with Fit Line
-    JFreeChart plotChart = ChartFactory.createScatterPlot(
-    "Track Data Plot",
-    "X-Axis", "Y-Axis", dataPoints);
+        // Create new series collection
+        XYSeriesCollection dataset = new XYSeriesCollection();
+        // Add series to series collection
+        dataset.addSeries(this.dataPoints);
+        dataset.addSeries(this.fitLine);
+        // Create dataset with series collection
+        XYDataset dataPoints = dataset;
+        // Create Scatter Plot with Fit Line
+        JFreeChart plotChart = ChartFactory.createScatterPlot("Track Data Plot","X-Axis", "Y-Axis", dataPoints);
 
-    /** Parameters for the chart, including size of draw area
-     free.org/jfreechart/javadoc/org/jfree/chart/ChartPanel
-     */
-    ChartPanel panel = new ChartPanel(plotChart, 600, 600, 600, 600, 600, 600,
+        /** Parameters for the chart, including size of draw area free.org/jfreechart/javadoc/org/jfree/chart/ChartPanel*/
+        ChartPanel panel = new ChartPanel(plotChart, 600, 600, 600, 600, 600, 600,
             true, false, false, false, false, false);
 
-    add(panel);
-    pack();
-    setTitle("Line chart");
-    XYPlot plot = plotChart.getXYPlot();
+        add(panel);
+        pack();
+        setTitle("Line chart");
+        XYPlot plot = plotChart.getXYPlot();
 
-    /** Create a renderer and alter how the series are displayed
-     jfree.org/jfreechart/javadoc/org/jfree/chart/renderer/xy/XYLineAndShapeRenderer
-     */
-    XYLineAndShapeRenderer renderer = new XYLineAndShapeRenderer();
-    renderer.setSeriesLinesVisible(0, false);
-    renderer.setSeriesLinesVisible(1, true);
-    renderer.setSeriesShapesVisible(0, true);
-    renderer.setSeriesShapesVisible(1, false);
-    renderer.setSeriesPaint(0, Color.RED);
-    renderer.setSeriesPaint(1, Color.BLUE);
-    renderer.setSeriesStroke(0, new BasicStroke(2.0f));
-    plot.setDomainGridlinesVisible(true);
-    plot.setDomainGridlinePaint(Color.BLACK);
+        /** Create a renderer and alter how the series are displayed
+        jfree.org/jfreechart/javadoc/org/jfree/chart/renderer/xy/XYLineAndShapeRenderer
+        */
+        XYLineAndShapeRenderer renderer = new XYLineAndShapeRenderer();
+        renderer.setSeriesLinesVisible(0, false);
+        renderer.setSeriesLinesVisible(1, true);
+        renderer.setSeriesShapesVisible(0, true);
+        renderer.setSeriesShapesVisible(1, false);
+        renderer.setSeriesPaint(0, Color.RED);
+        renderer.setSeriesPaint(1, Color.BLUE);
+        renderer.setSeriesStroke(0, new BasicStroke(2.0f));
+        plot.setDomainGridlinesVisible(true);
+        plot.setDomainGridlinePaint(Color.BLACK);
 
-    plot.setRenderer(renderer);
-    setLocationRelativeTo(null);
+        plot.setRenderer(renderer);
+        setLocationRelativeTo(null);
 
-    return plotChart;
+        return plotChart;
 
-  }
+    }
 
     /**
      * Adds the coordinates of the location data as points in the dataPoints series
@@ -125,88 +117,84 @@ public class TrackDataPlotter extends JFrame{
      * @param y float array of longitudes of location data points
      * @return XYSeries dataPoints representing location data points
      */
-  public XYSeries getTrackData(float[] x, float[] y){
-    XYSeries dataPoints = new XYSeries("Locations");
-    for(int i = 0; i < x.length; i++){
-      dataPoints.add(x[i], y[i]);
+    public XYSeries getTrackData(float[] x, float[] y) {
+        XYSeries dataPoints = new XYSeries("Locations");
+        for(int i = 0; i < x.length; i++){
+        dataPoints.add(x[i], y[i]);
+    }
+    return dataPoints;
     }
 
-    return dataPoints;
-  }
+    /**
+    * Calculates the line of best fit for the location points and represents it as a
+    * series containing two endpoints of a line segment spanning the x range of the data
+    * @param x float array of latitudes of location data points
+    * @param y float array of longitudes of location data points
+    * @return XYSeries fitLine representing a line of best fit for the location data points
+    */
+    private XYSeries calculateLine(float[] x, float[] y){
+        XYSeries fitLine = new XYSeries("Line");
+        float sumOfX = 0;
+        float sumOfY = 0;
+        float sumOfXy = 0;
+        float sumOfXSquared = 0;
+        double xMin = this.dataPoints.getMinX();
+        double xMax = this.dataPoints.getMaxX();
 
-  /**
-   * Calculates the line of best fit for the location points and represents it as a
-   * series containing two endpoints of a line segment spanning the x range of the data
-   * @param x float array of latitudes of location data points
-   * @param y float array of longitudes of location data points
-   * @return XYSeries fitLine representing a line of best fit for the location data points
-   */
-  private XYSeries calculateLine(float[] x, float[] y){
-      XYSeries fitLine = new XYSeries("Line");
-      float sumOfX = 0;
-      float sumOfY = 0;
-      float sumOfXy = 0;
-      float sumOfXSquared = 0;
-      double xMin = this.dataPoints.getMinX();
-      double xMax = this.dataPoints.getMaxX();
-
-      for(int i = 0; i < 5; i++) {
-    		sumOfXSquared += x[i] * x[i];
+        for(int i = 0; i < 5; i++) {
+            sumOfXSquared += x[i] * x[i];
     		sumOfX += x[i];
-    	  sumOfXy += x[i] * y[i];
-    	  sumOfY += y[i];
+    	    sumOfXy += x[i] * y[i];
+    	    sumOfY += y[i];
     	}
 
-      float topHalfOfEquation = 5 * sumOfXy - sumOfX * sumOfY;
-      float bottomHalfOfEquation = 5 * sumOfXSquared - sumOfX * sumOfX;
-      float slope = topHalfOfEquation/bottomHalfOfEquation;
-      float b = (sumOfY - (slope * sumOfX))/ 5;
+        float topHalfOfEquation = 5 * sumOfXy - sumOfX * sumOfY;
+        float bottomHalfOfEquation = 5 * sumOfXSquared - sumOfX * sumOfX;
+        float slope = topHalfOfEquation/bottomHalfOfEquation;
+        float b = (sumOfY - (slope * sumOfX))/ 5;
 
-      //Least Squares Regression = mx+b
-      fitLine.add(xMin, slope * xMin + b);
-      fitLine.add(xMax, slope * xMax + b);
+        //Least Squares Regression = mx+b
+        fitLine.add(xMin, slope * xMin + b);
+        fitLine.add(xMax, slope * xMax + b);
 
-      return fitLine;
-  }
-
-  /**
-   * Gets the latitudes of location data points from TrackData and stores a float array
-   * @param data TrackData object representing the location data to be plotted
-   * @return float[] latNum containing x coordinates of location data points
-   */
-  private float[] extractLatFromTrackData(TrackData data){
-    LocationDataPoint[] points = data.getLocationDataPoints();
-    float[] latNum = new float[5];
-    for(int i < points.length; i<=5; i++){
-      latNum[i]= points[i].getLat();
+        return fitLine;
     }
-    return latNum;
+
+    /**
+    * Gets the latitudes of location data points from TrackData and stores a float array
+    * @param data TrackData object representing the location data to be plotted
+    * @return float[] latNum containing x coordinates of location data points
+    */
+    private float[] extractLatFromTrackData(TrackData data) {
+        LocationDataPoint[] points = data.getLocationDataPoints();
+        float[] latNum = new float[5];
+        for(int i < points.length; i<=5; i++){
+            latNum[i]= points[i].getLat();
+        }
+        return latNum;
   }
 
-  /**
-   * Gets the longitudes of location data points from TrackData and stores a float array
-   * @param data TrackData object representing the location data to be plotted
-   * @return float[] lngNum containing y coordinates of location data points
-   */
-  private float[] extractLngFromTrackData(TrackData data){
-    LocationDataPoint[] points = data.getLocationDataPoints();
-    float[] lngNum = new float[5];
-    for(int i < points.length; i<=5; i++){
-      lngNum[i]= points[i].getLng();
+    /**
+    * Gets the longitudes of location data points from TrackData and stores a float array
+    * @param data TrackData object representing the location data to be plotted
+    * @return float[] lngNum containing y coordinates of location data points
+    */
+    private float[] extractLngFromTrackData(TrackData data) {
+        LocationDataPoint[] points = data.getLocationDataPoints();
+        float[] lngNum = new float[5];
+        for(int i < points.length; i<=5; i++) {
+            lngNum[i]= points[i].getLng();
+        }
+        return lngNum;
     }
-    return lngNum;
-  }
 
-  /**
-   * Displays the chart
-   */
-  public void displayChart(){
-      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      setVisible(true);
-  }
-
-
-
+    /**
+    * Displays the chart
+    */
+    public void displayChart(){
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
 }
 
 
