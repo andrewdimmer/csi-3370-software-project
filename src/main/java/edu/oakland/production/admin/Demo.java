9ae989f53f6326f48f64e1e32a31af5380000081
TessaPeruzzi <54461147+TessaPeruzzi@users.noreply.github.com>
2020-11-30 23:26:52 -0500

    [Admin] Build Demo

--- src/main/java/edu/oakland/production/admin/Demo.java
+++ src/main/java/edu/oakland/production/admin/Demo.java
@@ -1,8 +1,36 @@
 package edu.oakland.production.admin;
 
+import java.lang.IllegalArgumentException;
+import java.util.Scanner;
+
 public class Demo {
 
   public static void main(String[] args) {
     System.out.println("Hello World!");
+  }
+
+  public void initSecureLockTrackSystem(int systemRfid, String persistentStorageInputLocation, String persistentStorageOutputLocation) {
+    if (persistentStorageInputLocation == null) {
+        throw new IllegalArgumentException("Input Location cannot be null");
+    }  
+
+    if (persistentStorageOutputLocation == null) {
+      throw new IllegalArgumentException("Output Location cannot be null");
+    }
+
+    if (persistentStorageInputLocation.equals("")) {
+      throw new IllegalArgumentException("Input Location cannot be empty");
+    }  
+
+    if (persistentStorageOutputLocation.equals("")) {
+      throw new IllegalArgumentException("Output Location cannot be empty");
+    }  
+  }
+
+  public void runUseCases(Scanner input) {
+    if (input == null) {
+      throw new IllegalArgumentException("Scanner cannot be null");
+    } 
+
   }
 }
