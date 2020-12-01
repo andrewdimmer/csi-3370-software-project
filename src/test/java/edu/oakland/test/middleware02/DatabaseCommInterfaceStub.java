a0319e39b92cfc8f87f69dab9005f73b37a5609c
tomtomtomcat <55642071+tomtomtomcat@users.noreply.github.com>
2020-11-30 20:01:05 -0500

    fix indentation of DatabaseCommInterfaceStub

--- src/test/java/edu/oakland/test/middleware02/DatabaseCommInterfaceStub.java
+++ src/test/java/edu/oakland/test/middleware02/DatabaseCommInterfaceStub.java
@@ -5,23 +5,23 @@ import edu.oakland.helper.admin.TrackData;
 import edu.oakland.production.database.DatabaseCommInterface;
 
 public class DatabaseCommInterfaceStub implements DatabaseCommInterface {
-    private int rfid;
-    public DatabaseCommInterfaceStub(int rfid){
-        this.rfid = rfid;
-    }
-    public int receiveGetRfidRequest() {
-        return rfid;
-    }
-    public String receiveGetModeRequest() {
-        return null;
-    }
-    public void storeTrackDataRequest(TrackData trackData) {
-
-    }
-    public LocationDataPoint receiveGetLocationDataPointRequest(int offset) {
-        return null;
-    }
-    public TrackData receiveGetTrackDataRequest(int offset) {
-        return null;
-    }
+  private int rfid;
+  public DatabaseCommInterfaceStub(int rfid){
+    this.rfid = rfid;
   }
+  public int receiveGetRfidRequest() {
+    return rfid;
+  }
+  public String receiveGetModeRequest() {
+    return null;
+  }
+  public void storeTrackDataRequest(TrackData trackData) {
+      
+  }
+  public LocationDataPoint receiveGetLocationDataPointRequest(int offset) {
+    return null;
+  }
+  public TrackData receiveGetTrackDataRequest(int offset) {
+    return null;
+  }
+}
