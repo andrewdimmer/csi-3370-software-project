e3c3b2d7d6aa3acd28e54b8141973e5b589b355d
reedsikorskiOU <73550484+reedsikorskiOU@users.noreply.github.com>
2020-11-30 22:35:25 -0500

    help 2

--- src/main/java/edu/oakland/production/display01/DisplayGpsInterfaceImplementation.java
+++ src/main/java/edu/oakland/production/display01/DisplayGpsInterfaceImplementation.java
@@ -39,4 +39,5 @@ public class DisplayGpsInterfaceImplementation implements DisplayGpsInterface {
     System.out.println(satellite.getSatelliteName());
     return satellite.getSatelliteName();
   }
+  
 } 
