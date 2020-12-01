9b26fc87e0594f39cd51ddee848fb3bfa9617cf1
DLidz <46999371+DLidz@users.noreply.github.com>
2020-12-01 01:58:50 -0500

    Style guide fixes 2

--- src/test/java/edu/oakland/test/display01/DisplayGpsManagerImplementationTests.java
+++ src/test/java/edu/oakland/test/display01/DisplayGpsManagerImplementationTests.java
@@ -3,12 +3,12 @@ package edu.oakland.test.display01;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import edu.oakland.production.display01.DisplayGpsManagerImplementation;
 import edu.oakland.middleware01.MiddlewareGisInterface;
+import edu.oakland.production.display01.DisplayGpsManagerImplementation;
 import edu.oakland.production.middleware01.MiddlewareGisInterface;
+import edu.oakland.helper.admin.Satellite;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import edu.oakland.helper.admin.Satellite;
 
 @DisplayName("Display GPS Manager Unit Tests")
 public class DisplayGpsManagerImplementationTests {
