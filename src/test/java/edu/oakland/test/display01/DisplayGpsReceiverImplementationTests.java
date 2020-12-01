1e315aa260489a3ef8ba1208b519803e3c4e2204
EC01EC <73436762+EC01EC@users.noreply.github.com>
2020-11-30 22:29:26 -0500

    Update DisplayGpsReceiverImplementationTests.java
    Accepted changes

--- src/test/java/edu/oakland/test/display01/DisplayGpsReceiverImplementationTests.java
+++ src/test/java/edu/oakland/test/display01/DisplayGpsReceiverImplementationTests.java
@@ -1,24 +1,13 @@
-<<<<<<< Updated upstream
 package edu.oakland.test.display01;
-
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
-import edu.oakland.production.display01.DisplayGpsReceiverImplementation;
-=======
-  
-package edu.oakland.test.display01;
-
-import edu.oakland.helper.admin.Satellite;
-import edu.oakland.helper.display01.SatelliteSignalCheckRequest;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import edu.oakland.helper.admin.Satellite;
+import edu.oakland.helper.display01.SatelliteSignalCheckRequest;
 import edu.oakland.production.display01.DisplayGpsReceiver;
 import edu.oakland.production.display01.DisplayGpsReceiverImplementation;
 import edu.oakland.test.display01.DisplayGpsManagerStub;
-
->>>>>>> Stashed changes
 import java.lang.IllegalArgumentException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -34,9 +23,7 @@ public class DisplayGpsReceiverImplementationTests {
       new DisplayGpsReceiverImplementation(null);
     });
   }
-<<<<<<< Updated upstream
-}
-=======
+
   @Test
   @DisplayName ("Gps Signal Strength Signal Is Not Null")
   public void measureGpsSignalStrengthSatelliteSignalIsNotNull(){
@@ -44,18 +31,18 @@ public class DisplayGpsReceiverImplementationTests {
       new DisplayGpsReceiverImplementation(null);
     });
   }
+
   @Test
   @DisplayName ("Satellite Signal Is Not Null")
   public void measureSignalSatelliteSignalIsNotNull(){
     assertThrows(IllegalArgumentException.class, () -> {
       new DisplayGpsReceiverImplementation(null);
-  });
+    });
+  }
 
   @Test
   @DisplayName ("Gps Signal Strength In Is Signal Strength Out ")
   public void measureGpsSignalStrengthSatelliteInIsSignalOut(Satellite satelliteSignal) {
-
-  
     DisplayGpsManagerStub stub = new DisplayGpsManagerStub();
     stub.receiveGpsSignalStrength(satelliteSignal);
     DisplayGpsReceiver gpsReceiver = new DisplayGpsReceiverImplementation(stub);
@@ -67,4 +54,3 @@ public class DisplayGpsReceiverImplementationTests {
     
   }
 }
->>>>>>> Stashed changes
