d59df7087c994c7e767df4efabb2b7d54b21f64c
Sydney Hill <sydneyhill3901@gmail.com>
2020-12-02 11:06:38 -0500

    Style Changes

--- src/test/java/edu/oakland/test/display02/TrackDataPlotterTests.java
+++ src/test/java/edu/oakland/test/display02/TrackDataPlotterTests.java
@@ -36,7 +36,7 @@ public class TrackDataPlotterTests {
   @DisplayName("Invalid TrackData is not permitted")
   void invalidTrackDataNotAllowed() {
     Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
-        new TrackDataPlotter(generateInvalidTrackData());
+      new TrackDataPlotter(generateInvalidTrackData());
     });
     assertEquals("Not enough points to create a valid TrackData object", exception.getMessage());
   }
