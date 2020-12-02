0138e1d2c463c5d25931c955a1b264fa2edf3886
Andrew Dimmer <contactandrewd@gmail.com>
2020-12-02 17:48:55 -0500

    [Admin] Build and Test System Initialization

--- src/main/java/edu/oakland/production/admin/Demo.java
+++ src/main/java/edu/oakland/production/admin/Demo.java
@@ -1,38 +1,152 @@
 package edu.oakland.production.admin;
 
+import edu.oakland.helper.admin.LocationDataPoint;
+import edu.oakland.helper.admin.TrackData;
+import edu.oakland.production.database.DatabaseCommInterface;
+import edu.oakland.production.database.DatabaseCommInterfaceImplementation;
+import edu.oakland.production.database.DatabaseCommManager;
+import edu.oakland.production.database.DatabaseCommManagerImplementation;
+import edu.oakland.production.database.DatabaseGisInterface;
+import edu.oakland.production.database.DatabaseGisInterfaceImplementation;
+import edu.oakland.production.database.DatabaseGisManager;
+import edu.oakland.production.database.DatabaseGisManagerImplementation;
+import edu.oakland.production.database.DatabasePersistentStorage;
+import edu.oakland.production.database.DatabasePersistentStorageImplementation;
+import edu.oakland.production.display01.DisplayGpsInterface;
+import edu.oakland.production.display01.DisplayGpsInterfaceImplementation;
+import edu.oakland.production.display01.DisplayGpsManager;
+import edu.oakland.production.display01.DisplayGpsManagerImplementation;
+import edu.oakland.production.display01.DisplayGpsReceiver;
+import edu.oakland.production.display01.DisplayGpsReceiverImplementation;
+import edu.oakland.production.display02.DisplayComm2WayInterface;
+import edu.oakland.production.display02.DisplayComm2WayInterfaceImplementation;
+import edu.oakland.production.display02.DisplayCommInterface;
+import edu.oakland.production.display02.DisplayCommInterfaceImplementation;
+import edu.oakland.production.display02.DisplayCommManager;
+import edu.oakland.production.display02.DisplayCommManagerImplementation;
+import edu.oakland.production.middleware01.MiddlewareGisInterface;
+import edu.oakland.production.middleware01.MiddlewareGisInterfaceImplementation;
+import edu.oakland.production.middleware01.MiddlewareGisManager;
+import edu.oakland.production.middleware01.MiddlewareGisManagerImplementation;
+import edu.oakland.production.middleware01.MiddlewareGisProcessor;
+import edu.oakland.production.middleware01.MiddlewareGisProcessorImplementation;
+import edu.oakland.production.middleware02.MiddlewareCommInterface;
+import edu.oakland.production.middleware02.MiddlewareCommInterfaceImplementation;
+import edu.oakland.production.middleware02.MiddlewareCommLinkManager;
+import edu.oakland.production.middleware02.MiddlewareCommLinkManagerImplementation;
+import edu.oakland.production.middleware02.MiddlewareCommDatabaseInterface;
+import edu.oakland.production.middleware02.MiddlewareCommDatabaseInterfaceImplementation;
 import java.lang.IllegalArgumentException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Demo {
 
-  public static void main(String[] args) {
-    System.out.println("Hello World!");
+  private GpsSystem gpsSystem;
+  private User user;
+
+  public int configureRfid(Scanner input) {
+    if (input == null) {
+      throw new IllegalArgumentException("Scanner cannot be null");
+    }
+    System.out.println("Enter the RFID number of the shipping container that this Secure Lock");
+    System.out.println("Tracking System unit is attached to:");
+    return input.nextInt();
+  }
+
+  public String[] configureSatelliteNames(Scanner input) {
+    if (input == null) {
+      throw new IllegalArgumentException("Scanner cannot be null");
+    }
+    System.out.println("Enter the number of satellites in range:");
+    int numberOfSatellites = input.nextInt();
+    input.nextLine(); // Eat new line character
+    String[] satelliteNames = new String[numberOfSatellites];
+    for (int index = 0; index < numberOfSatellites; index++) {
+      System.out.println("Enter the name of satellite " + (index + 1) + ":");
+      satelliteNames[index] = input.nextLine();
+    }
+    return satelliteNames;
   }
   
   /**
    * Initiates the Secure Lock Track System.
    *
-   * @param systemRfid The rfid number
-   * @param persistentStorageInputLocation The input location.
-   * @param persistentStorageOutputLocation The output location.
+   * @param systemRfid The rfid number.
+   * @param satelliteNames The list of satellite names.
    */
-  public void initSecureLockTrackSystem(int systemRfid, String persistentStorageInputLocation, 
-      String persistentStorageOutputLocation) {
-    if (persistentStorageInputLocation == null) {
-      throw new IllegalArgumentException("Input Location cannot be null");
-    }  
+  public void initSecureLockTrackSystem(int systemRfid, String[] satelliteNames) {
+    // Stand Up Database
+    DatabasePersistentStorage database3 = new DatabasePersistentStorageImplementation(
+      systemRfid,
+      new ArrayList<LocationDataPoint>(),
+      new ArrayList<TrackData>(),
+      satelliteNames
+    );
+    DatabaseGisManager database01_2 = new DatabaseGisManagerImplementation(database3);
+    DatabaseCommManager database02_2 = new DatabaseCommManagerImplementation(database3);
+    DatabaseGisInterface database01_1 = new DatabaseGisInterfaceImplementation(database01_2);
+    DatabaseCommInterface database02_1 = new DatabaseCommInterfaceImplementation(database02_2);
 
-    if (persistentStorageOutputLocation == null) {
-      throw new IllegalArgumentException("Output Location cannot be null");
-    }
+    // Stand Up Middleware01
+    MiddlewareGisManager middleware01_3 = new MiddlewareGisManagerImplementation(database01_1);
+    MiddlewareGisProcessor middleware01_2 = new MiddlewareGisProcessorImplementation(
+        middleware01_3
+    );
+    MiddlewareGisInterface middleware01_1 = new MiddlewareGisInterfaceImplementation(
+        middleware01_2
+    );
 
-    if (persistentStorageInputLocation.equals("")) {
-      throw new IllegalArgumentException("Input Location cannot be empty");
-    }  
+    // Stand Up Middleware02
+    MiddlewareCommDatabaseInterface middleware02_3 =
+        new MiddlewareCommDatabaseInterfaceImplementation(database02_1);
+    MiddlewareCommLinkManager middleware02_2 = new MiddlewareCommLinkManagerImplementation(
+        middleware02_3,
+        middleware01_3
+    );
+    MiddlewareCommInterface middleware02_1 = new MiddlewareCommInterfaceImplementation(
+        middleware02_2
+    );
 
-    if (persistentStorageOutputLocation.equals("")) {
-      throw new IllegalArgumentException("Output Location cannot be empty");
-    }  
+    // Stand Up Display01
+    DisplayGpsManager display01_3 = new DisplayGpsManagerImplementation(middleware01_1);
+    DisplayGpsReceiver display01_2 = new DisplayGpsReceiverImplementation(
+        display01_3
+    );
+    DisplayGpsInterface display01_1 = new DisplayGpsInterfaceImplementation(
+        display01_2
+    );
+
+    // Stand Up Display02
+    DisplayCommManager display02_3 = new DisplayCommManagerImplementation(middleware02_1);
+    DisplayComm2WayInterface display02_2 = new DisplayComm2WayInterfaceImplementation(
+        display02_3
+    );
+    DisplayCommInterface display02_1 = new DisplayCommInterfaceImplementation(
+        display02_2
+    );
+
+    // Stand Up End User Classes
+    gpsSystem = new GpsSystem(display01_1, satelliteNames);
+    user = new User(display02_1);
+  }
+
+  /**
+   * Exposes the current GpsSystem for testing and verification. Not to be used for production.
+   *
+   * @return The GpsSystem object stored in the current instance of the Demo class.
+   */
+  public GpsSystem getGpsSystem() {
+    return gpsSystem;
+  }
+
+  /**
+   * Exposes the current User for testing and verification. Not to be used for production.
+   *
+   * @return The User object stored in the current instance of the Demo class.
+   */
+  public User getUser() {
+    return user;
   }
   
   /**
@@ -44,6 +158,15 @@ public class Demo {
     if (input == null) {
       throw new IllegalArgumentException("Scanner cannot be null");
     } 
-
   }
+
+  public static void main(String[] args) {
+    System.out.println("Welcome to the Secure Lock Tracking Software!");
+    Demo demo = new Demo();
+    Scanner scanner = new Scanner(System.in);
+    int rfid = demo.configureRfid(scanner);
+    String[] satelliteNames = demo.configureSatelliteNames(scanner);
+    demo.initSecureLockTrackSystem(rfid, satelliteNames);
+  }
+
 }
--- src/test/java/edu/oakland/test/admin/DemoTests.java
+++ src/test/java/edu/oakland/test/admin/DemoTests.java
@@ -1,73 +1,79 @@
 package edu.oakland.test.admin;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import edu.oakland.production.admin.Demo;
 import java.lang.IllegalArgumentException;
 import java.util.Random;
+import java.util.Scanner;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 @DisplayName("Demo Unit Tests")
 public class DemoTests { 
 
-
   @Test
-  @DisplayName("Storage Input Location is Not Null")
-  void storageInputLocIsNotNull() {
-    int rfid = generateRandomNumber();
-    int stringLength = generateRandomStringLength();
-    String output = generateRandomString(stringLength);
+  @DisplayName("configureRfid Scanner Input is Not Null")
+  void configureRfidScannerInputIsUserInput() {
     Demo demo = new Demo();
     assertThrows(IllegalArgumentException.class, () -> {
-      demo.initSecureLockTrackSystem(rfid, null, output);
+      demo.configureRfid(null);
     });
   }
 
   @Test
-  @DisplayName("Storage Output Location is Not Null")
-  void storageOutputLocIsNotNull() {
+  @DisplayName("configureRfid Rfid In is Rfid Out")
+  void configureRfidRfidInIsRfidOut() {
+    Demo demo = new Demo();
     int rfid = generateRandomNumber();
-    int stringLength = generateRandomStringLength();
-    String input = generateRandomString(stringLength);
+    assertEquals(rfid, demo.configureRfid(new Scanner(String.valueOf(rfid))));
+  }
+
+  @Test
+  @DisplayName("configureSatelliteNames Scanner Input is Not Null")
+  void configureSatelliteNamesScannerInputIsUserInput() {
     Demo demo = new Demo();
     assertThrows(IllegalArgumentException.class, () -> {
-      demo.initSecureLockTrackSystem(rfid, input, null);
+      demo.configureSatelliteNames(null);
     });
   }
 
   @Test
-  @DisplayName("Storage Input Location is Not Empty")
-  void storageInputLocIsNotEmpty() {
-    int rfid = generateRandomNumber();
-    int stringLength = generateRandomStringLength();
-    String output = generateRandomString(stringLength);
+  @DisplayName("configureSatelliteNames Satellite Names In are Satellite Names")
+  void configureSatelliteNamesSatelliteNamesInAreSatelliteNamesOut() {
     Demo demo = new Demo();
-    assertThrows(IllegalArgumentException.class, () -> {
-      demo.initSecureLockTrackSystem(rfid, "", output);
-    });
+    int count = generateRandomNumber();
+    String[] inputSatelliteNames = new String[count];
+    String inputString = String.valueOf(count) + "\n";
+    for (int index = 0; index < count; index++) {
+      String satelliteName = generateRandomString(generateRandomStringLength());
+      inputSatelliteNames[index] = satelliteName;
+      inputString += satelliteName + "\n";
+    }
+    assertArrayEquals(inputSatelliteNames, demo.configureSatelliteNames(new Scanner(inputString)));
   }
 
   @Test
-  @DisplayName("Storage Output Location is Not Empty")
-  void storageOutputLocIsNotEmpty() {
-    int rfid = generateRandomNumber();
-    int stringLength = generateRandomStringLength();
-    String input = generateRandomString(stringLength);
-    Demo demo = new Demo();
-    assertThrows(IllegalArgumentException.class, () -> {
-      demo.initSecureLockTrackSystem(rfid, input, "");
-    });
-  }
-
-  @Test
-  @DisplayName("Scanner Input is Not Null")
-  void scannerInputIsUserInput() {
+  @DisplayName("runUseCases Scanner Input is Not Null")
+  void runUseCasesScannerInputIsUserInput() {
     Demo demo = new Demo();
     assertThrows(IllegalArgumentException.class, () -> {
       demo.runUseCases(null);
     });
+  }
+
+  @Test
+  @DisplayName("Secure Lock Tracking System Initialization Successful")
+  void initSuccessful() {
+    Demo demo = new Demo();
+    demo.initSecureLockTrackSystem(
+        generateRandomNumber(),
+        generateRandomStringArray(generateRandomStringLength())
+    );
+    assertTrue(demo.getGpsSystem() != null && demo.getUser() != null);
   }
 
   private int generateRandomNumber() {
@@ -76,6 +82,14 @@ public class DemoTests {
 
   private int generateRandomStringLength() {
     return (int) ((Math.random() * (20 - 1)) + 1); 
+  }
+
+  private String[] generateRandomStringArray(int length) { 
+    String[] strings = new String[length];
+    for (int index = 0; index < strings.length; index++) {
+      strings[index] = generateRandomString(generateRandomStringLength());
+    }
+    return strings;
   }
 
   private String generateRandomString(int length) { 
