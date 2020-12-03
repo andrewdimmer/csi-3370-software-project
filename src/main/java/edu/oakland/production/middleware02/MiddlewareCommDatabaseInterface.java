0e685591d055efb496ef05136c02215e48b52761
tomtomtomcat <55642071+tomtomtomcat@users.noreply.github.com>
2020-12-03 02:48:31 -0500

    style guide fixes for MiddlewareCommDatabaseInterface

--- src/main/java/edu/oakland/production/middleware02/MiddlewareCommDatabaseInterface.java
+++ src/main/java/edu/oakland/production/middleware02/MiddlewareCommDatabaseInterface.java
@@ -1,22 +1,16 @@
 package edu.oakland.production.middleware02;
 
-import edu.oakland.helper.admin.TrackData;
 import edu.oakland.helper.admin.LocationDataPoint;
+import edu.oakland.helper.admin.TrackData;
 
 public interface MiddlewareCommDatabaseInterface {
 
   public int checkCurrentRfid();
   
-
   public String requestMode(); 
   
   public TrackData getTrackData();
 
   public LocationDataPoint getLocationDataPoint(int offset);
 
-
-
-  
-  
-  
 } 
